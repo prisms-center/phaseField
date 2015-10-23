85a3a557ff6017cbea1043a6adf4806edae69cd1
Stephen DeWitt <stvdwtt@umich.edu>
2015-10-23 15:01:07 -0400

    changed grid generation to subdivided hyper rectangle, added subdivision parameters to all application input files, and changed grid spacing in the main.cc files

--- src/matrixfree/init.cc
+++ src/matrixfree/init.cc
@@ -12,13 +12,23 @@
    computing_timer.enter_section("matrixFreePDE: initialization"); 
 
    //creating mesh
+   std::vector<unsigned int> subdivisions;
+   subdivisions.push_back(subdivisionsX);
+   if (dim>1){
+	   subdivisions.push_back(subdivisionsY);
+	   if (dim>2){
+		   subdivisions.push_back(subdivisionsZ);
+	   }
+   }
+   GridGenerator::subdivided_hyper_rectangle (triangulation, subdivisions, Point<dim>(), Point<dim>(spanX,spanY));
+
    pcout << "creating problem mesh...\n";
  #if problemDIM==3
-   GridGenerator::hyper_rectangle (triangulation, Point<dim>(), Point<dim>(spanX,spanY,spanZ));
+   GridGenerator::subdivided_hyper_rectangle (triangulation, subdivisions, Point<dim>(), Point<dim>(spanX,spanY,spanZ));
 #elif problemDIM==2
-   GridGenerator::hyper_rectangle (triangulation, Point<dim>(), Point<dim>(spanX,spanY));
+   GridGenerator::subdivided_hyper_rectangle (triangulation, subdivisions, Point<dim>(), Point<dim>(spanX,spanY));
  #elif problemDIM==1
-   GridGenerator::hyper_rectangle (triangulation, Point<dim>(), Point<dim>(spanX));
+   GridGenerator::subdivided_hyper_rectangle (triangulation, subdivisions, Point<dim>(), Point<dim>(spanX));
  #endif
    triangulation.refine_global (refineFactor);
    //write out extends
