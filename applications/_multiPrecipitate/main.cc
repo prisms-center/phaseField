c57645f16b48cec7aebac6754edd58728bb2654a
Stephen DeWitt <stvdwtt@umich.edu>
2015-11-17 17:00:19 -0500

    Reduced supersaturation in _multiPrecipitate to 0.4%, as seen experimentally, this removes the halo artifact

---
+++ applications/_multiPrecipitate/main.cc
@@ -0,0 +1,401 @@
+//Coupled Cahn-Hilliard and Allen-Cahn implementation with nucleation
+//general headers
+#include "../../include/dealIIheaders.h"
+//coupled Cahn-Hilliard and Allen-Cahn problem headers
+#include "parameters.h"
+#include "../../src/models/coupled/coupledCHACMechanics.h"
+#include <time.h>
+
+
+//structure representing each nucleus 
+struct nucleus{
+  unsigned int index;
+  dealii::Point<problemDIM> center;
+  double radius;
+  double seededTime, seedingTime;
+};
+//vector of all nucleus seeded in the problem
+std::vector<nucleus> nuclei, localNuclei;
+
+//initial condition function for concentration
+template <int dim>
+class InitialConditionC : public Function<dim>
+{
+public:
+  InitialConditionC () : Function<dim>(1) {
+    //std::srand(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)+1);
+    std::srand(time(NULL));
+  }
+  double value (const Point<dim> &p, const unsigned int component = 0) const
+  {
+    //return the value of the initial concentration field at point p 
+    return avg_Nd;
+  }
+};
+
+//initial condition function for the order parameters
+template <int dim>
+class InitialConditionN : public Function<dim>
+{
+public:
+  unsigned int index;
+  InitialConditionN (const unsigned int _index) : Function<dim>(1), index(_index) {
+    std::srand(Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)+1);
+  }
+  double value (const Point<dim> &p, const unsigned int component = 0) const
+  {
+    //set result equal to the structural order paramter initial condition
+	  double dx=spanX/( (double)subdivisionsX )/std::pow(2.0,refineFactor);
+	  double dy=spanY/( (double)subdivisionsY )/std::pow(2.0,refineFactor);
+	  double dz=spanZ/( (double)subdivisionsZ )/std::pow(2.0,refineFactor);
+    double r=0.0;
+	#if problemDIM==1
+    r=p.operator()(0);
+	  return 0.5*(1.0-std::tanh((r-spanX/16.0)/(0.1*dx)));
+	#elif problemDIM==2
+	  if (index==1){
+		  //r=sqrt((p.operator()(0)-spanX/2.0)*(p.operator()(0)-spanX/2.0)+(p.operator()(1)-spanY/2.0)*(p.operator()(1)-spanY/2.0));
+		  //return 0.5*(1.0-std::tanh((r-spanX/20.0)/(2.0*dx)));
+		  return 0.0;
+	  }
+	  else if (index==2){
+		return 0.0;
+	  }
+	  else if (index==3){
+		return 0.0;
+	  }
+	  //return 0.5*(1.0-std::tanh((r-spanX/16.0)/(3*dx)));
+	#elif problemDIM==3
+	  if (index==1){
+	  //r=p.distance(Point<dim>(spanX/2.0,spanY/2.0,spanZ/2.0));
+	  //return 0.5*(1.0-std::tanh((r-spanX/8.0)/(3*dx)));
+		  r=sqrt((p.operator()(0)-spanX/2.0)*(p.operator()(0)-spanX/2.0)
+		  		+(p.operator()(1)-spanY/2.0)*(p.operator()(1)-spanY/2.0)
+		  		+(p.operator()(2)-spanZ/2.0)*(p.operator()(2)-spanZ/2.0));
+		return 0.5*(1.0-std::tanh((r-2.3811)/(1.0*dz)));
+	  }
+	  else if (index==2){
+		return 0.0;
+	  }
+	  else if (index==3){
+		return 0.0;
+	  }
+	#endif
+	return 0.0;
+  }
+};
+
+//apply initial conditions
+template <int dim>
+void CoupledCHACMechanicsProblem<dim>::applyInitialConditions()
+{
+  unsigned int fieldIndex;
+  
+  //call initial condition function for c
+  fieldIndex=this->getFieldIndex("c");
+  VectorTools::interpolate (*this->dofHandlersSet[fieldIndex],		\
+			    InitialConditionC<dim>(),			\
+			    *this->solutionSet[fieldIndex]);
+  //call initial condition function for structural order parameters
+  fieldIndex=this->getFieldIndex("n1");
+  VectorTools::interpolate (*this->dofHandlersSet[fieldIndex], InitialConditionN<dim>(1), *this->solutionSet[fieldIndex]);
+  fieldIndex=this->getFieldIndex("n2");
+  VectorTools::interpolate (*this->dofHandlersSet[fieldIndex], InitialConditionN<dim>(2), *this->solutionSet[fieldIndex]);
+  fieldIndex=this->getFieldIndex("n3");
+  VectorTools::interpolate (*this->dofHandlersSet[fieldIndex], InitialConditionN<dim>(3), *this->solutionSet[fieldIndex]);
+  //set zero intial condition for u
+  fieldIndex=this->getFieldIndex("u");
+  *this->solutionSet[fieldIndex]=0.0;
+}
+
+//nucleation model implementation
+template <int dim>
+void CoupledCHACMechanicsProblem<dim>::modifySolutionFields()
+{
+  //current time
+  double t=this->currentTime;
+  unsigned int inc=this->currentIncrement;
+  double dx=spanX/( (double)subdivisionsX )/std::pow(2.0,refineFactor);
+  double rand_val;
+  int count = 0;
+  //nucleation parameters
+  double nRadius = 2.5; //spanX/20.0;
+  double minDistBetwenNuclei=4*nRadius;
+
+  unsigned int maxNumberNuclei=5; // doesn't do anything currently
+
+  //get the list of node points in the domain
+  std::map<dealii::types::global_dof_index, dealii::Point<dim> > support_points;
+  dealii::DoFTools::map_dofs_to_support_points (dealii::MappingQ1<dim>(), *this->dofHandlersSet[0], support_points);   
+  //fields
+  vectorType* n1=this->solutionSet[this->getFieldIndex("n1")];
+  vectorType* n2=this->solutionSet[this->getFieldIndex("n2")];
+  vectorType* n3=this->solutionSet[this->getFieldIndex("n3")];
+  vectorType* c=this->solutionSet[this->getFieldIndex("c")];
+  const double k1 = 0.0001; // nucleation probability constant
+  const double k2 = 1.0;	// nucleation probability constant
+  const double c0 = 0.300;	// baseline concentration?
+  double J = 0.0;
+  //delete the previous entries in the nuclei vector (old nucleus are still retained in the localNuclei vector)
+  nuclei.clear();
+  
+  //populate localNuclei vector
+  if (inc <= timeIncrements){
+    nucleus* temp;
+    //add nuclei based on concentration field values
+    //loop over all points in the domain
+    for (typename std::map<dealii::types::global_dof_index, dealii::Point<dim> >::iterator it=support_points.begin(); it!=support_points.end(); ++it){
+      unsigned int dof=it->first;
+      //set only local owned values of the parallel vector (eventually turn this into a separate function for each order parameter)
+      if (n1->locally_owned_elements().is_element(dof)){
+    	  dealii::Point<dim> nodePoint=it->second;
+    	  double n1Value=(*n1)(dof);
+    	  double n2Value=(*n2)(dof);
+    	  double n3Value=(*n3)(dof);
+    	  double cValue=(*c)(dof);
+	
+    	  rand_val = (double)rand()/(RAND_MAX);
+
+    	  if ((t > 1000000000*timeStep) || (n1Value+n2Value+n3Value > 1.0e-6) || (cValue <= 0.0)) {
+    		  J = 0;
+    	  }
+		  else{
+			  J = cValue/avg_Nd * dx*dx/((double)spanX * (double)spanY) * 0.01;
+    	  }
+
+    	  if (rand_val <= J){
+    		  bool isClose=false;
+    		  for (std::vector<nucleus>::iterator thisNuclei=localNuclei.begin(); thisNuclei!=localNuclei.end(); ++thisNuclei){
+    			  if (thisNuclei->center.distance(nodePoint)<minDistBetwenNuclei){
+    				  isClose=true;
+    			  }
+    		  }
+
+    		  if (!isClose){
+    			  temp = new nucleus;
+    			  temp->index=localNuclei.size();
+    			  temp->center=nodePoint;
+    			  temp->radius=nRadius;
+    			  temp->seededTime=t;
+    			  temp->seedingTime = 10000.0*timeStep;
+    			  localNuclei.push_back(*temp);
+    		  }
+    	  }
+      }
+    }
+
+
+    //filter nuclei by comparing with other processors
+    int numProcs=Utilities::MPI::n_mpi_processes(MPI_COMM_WORLD);        
+    int thisProc=Utilities::MPI::this_mpi_process(MPI_COMM_WORLD);
+    std::vector<int> numNucleiInProcs(numProcs, 0);
+    //send nuclei information to processor 0
+    int numNuclei=localNuclei.size();
+    //send information about number of nuclei to processor 0
+    if (thisProc!=0){
+      MPI_Send(&numNuclei, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
+    else{
+      numNucleiInProcs[0]=numNuclei;
+      for (int proc=1; proc<numProcs; proc++){
+	MPI_Recv(&numNucleiInProcs[proc], 1, MPI_INT, proc, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    //filter nuclei in processor zero
+    //receive nuclei info from all processors
+    if (thisProc!=0){
+    	if (numNuclei>0){
+    		std::vector<double> tempData((dim+3)*numNuclei);
+    		unsigned int i=0;
+    		for (std::vector<nucleus>::iterator thisNuclei=localNuclei.begin(); thisNuclei!=localNuclei.end(); ++thisNuclei){
+    			tempData[i*(dim+3)]=thisNuclei->radius;
+    			tempData[i*(dim+3)+1]=thisNuclei->seededTime;
+    			tempData[i*(dim+3)+2]=thisNuclei->seedingTime;
+    			for (unsigned int j=0; j<dim; j++) tempData[i*(dim+3)+3+j]=thisNuclei->center[j];
+    			i++;
+    		}
+    		MPI_Send(&tempData[0], numNuclei*(dim+3), MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
+    	}
+    }
+    else{
+    	//temporary array to store all the nuclei
+    	std::vector<std::vector<double>*> tempNuceli(numProcs);
+    	for (int proc=0; proc<numProcs; proc++) {
+    		std::vector<double>* temp=new std::vector<double>(numNucleiInProcs[proc]*(dim+3));
+    		if (numNucleiInProcs[proc]>0){
+    			if (proc==0){
+    				unsigned int i=0;
+    				for (std::vector<nucleus>::iterator thisNuclei=localNuclei.begin(); thisNuclei!=localNuclei.end(); ++thisNuclei){
+    					(*temp)[i*(dim+3)]=thisNuclei->radius;
+    					(*temp)[i*(dim+3)+1]=thisNuclei->seededTime;
+    					(*temp)[i*(dim+3)+2]=thisNuclei->seedingTime;
+    					for (unsigned int j=0; j<dim; j++) (*temp)[i*(dim+3)+3+j]=thisNuclei->center[j];
+    					i++;
+    				}
+    			}
+    			else{
+    				MPI_Recv(&((*temp)[0]), numNucleiInProcs[proc]*(dim+3), MPI_DOUBLE, proc, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    			}
+    			tempNuceli[proc]=temp;
+    		}
+    	}
+    
+    	//filter the nuclei and add to nuclei vector in processor zero
+    	for (int proc1=0; proc1<numProcs; proc1++) {
+    		for (int i1=0; i1<numNucleiInProcs[proc1]; i1++){
+    			double rad1=(*tempNuceli[proc1])[i1*(dim+3)];
+    			double time1=(*tempNuceli[proc1])[i1*(dim+3)+1];
+    			double seedingTime1=(*tempNuceli[proc1])[i1*(dim+3)+2];
+    			dealii::Point<dim> center1;
+    			for (unsigned int j1=0; j1<dim; j1++) {
+    				center1[j1]=(*tempNuceli[proc1])[i1*(dim+3)+3+j1];
+    			}
+    			bool addNuclei=true;
+    			//check if this nuceli present in any other processor
+    			for (int proc2=0; proc2<numProcs; proc2++) {
+    				if (proc1!=proc2){
+    					for (int i2=0; i2<numNucleiInProcs[proc2]; i2++){
+    						double rad2=(*tempNuceli[proc2])[i2*(dim+3)];
+    						double time2=(*tempNuceli[proc2])[i2*(dim+3)+1];
+    						dealii::Point<dim> center2;
+    						for (unsigned int j2=0; j2<dim; j2++) center2(j2)=(*tempNuceli[proc2])[i2*(dim+3)+3+j2];
+    						if ((center1.distance(center2)<=minDistBetwenNuclei) && (time1>=time2)){
+    							addNuclei=false;
+    							break;
+    						}
+    					}
+    					if (!addNuclei) {break;}
+    				}
+    			}
+    			if (addNuclei){
+    				temp = new nucleus;
+    				temp->index=nuclei.size();
+    				temp->radius=rad1;
+    				temp->seededTime=time1;
+    				temp->seedingTime=seedingTime1;
+    				temp->center=center1;
+    				nuclei.push_back(*temp);
+    			}
+    		}
+    	}
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //disperse nuclei to all other processors
+    int numGlobalNuclei;
+    if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)==0) {numGlobalNuclei=nuclei.size();}
+    MPI_Bcast(&numGlobalNuclei, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    this->pcout << "total number of nuclei currently seeded : "  << numGlobalNuclei << std::endl;
+    MPI_Barrier(MPI_COMM_WORLD);
+    //
+    std::vector<double> temp2(numGlobalNuclei*(dim+3));
+    if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)==0){
+      unsigned int i=0;
+      for (std::vector<nucleus>::iterator thisNuclei=nuclei.begin(); thisNuclei!=nuclei.end(); ++thisNuclei){
+	temp2[i*(dim+3)]=thisNuclei->radius;
+	temp2[i*(dim+3)+1]=thisNuclei->seededTime;
+	temp2[i*(dim+3)+2]=thisNuclei->seedingTime;
+	for (unsigned int j=0; j<dim; j++) temp2[i*(dim+3)+3+j]=thisNuclei->center[j];
+	i++;
+      }
+    }
+    MPI_Bcast(&temp2[0], numGlobalNuclei*(dim+3), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Barrier(MPI_COMM_WORLD);
+    //receive all nuclei
+    if (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD)!=0){
+    	for(unsigned int i=0; i<numGlobalNuclei; i++){
+    		temp = new nucleus;
+    		temp->index=nuclei.size();
+    		temp->radius=temp2[i*(dim+3)];
+    		temp->seededTime=temp2[i*(dim+3)+1];
+    		temp->seedingTime=temp2[i*(dim+3)+2];
+    		dealii::Point<dim> tempCenter;
+    		for (unsigned int j=0; j<dim; j++) tempCenter[j]=temp2[i*(dim+3)+3+j];
+    		temp->center=tempCenter;
+    		nuclei.push_back(*temp);
+      }
+    }
+  }
+
+  //seed nuclei
+  unsigned int fieldIndex=this->getFieldIndex("n1");
+  for (std::vector<nucleus>::iterator thisNuclei=nuclei.begin(); thisNuclei!=nuclei.end(); ++thisNuclei){
+
+	  dealii::Point<dim> center=thisNuclei->center;
+	  double radius=thisNuclei->radius;
+	  double seededTime=thisNuclei->seededTime;
+	  double seedingTime=thisNuclei->seedingTime;
+	  this->pcout << "times: " << t << " " << seededTime << " " << seedingTime << std::endl;
+	  //loop over all points in the domain
+	  for (typename std::map<dealii::types::global_dof_index, dealii::Point<dim> >::iterator it=support_points.begin(); it!=support_points.end(); ++it){
+		  unsigned int dof=it->first;
+		  //set only local owned values of the parallel vector
+		  if (n1->locally_owned_elements().is_element(dof)){
+			  dealii::Point<dim> nodePoint=it->second;
+			  //check conditions and seed nuclei
+			  double r=nodePoint.distance(center);
+			  if (r<=(2*radius)){
+				  if ((t>seededTime) && (t<(seededTime+seedingTime))){
+					  //this->pcout << "times: " << t << " " << seededTime << " " << seedingTime << std::endl;
+					  //(*n1)(dof)=0.5*(1.0-std::tanh((r-radius)/(dx)));
+					  (*n1)(dof)=0.5*(1.0-std::tanh((r-radius)/(0.4)));
+				  }
+			  }
+		  }
+	  }
+  }
+}
+
+//apply Dirchlet BC function
+template <int dim>
+void CoupledCHACMechanicsProblem<dim>::applyDirichletBCs(){
+  //Set u=0 at all boundaries
+  VectorTools::interpolate_boundary_values (*this->dofHandlersSet[this->getFieldIndex("u")],\
+					    0, ZeroFunction<dim>(dim), *(ConstraintMatrix*) \
+					    this->constraintsSet[this->getFieldIndex("u")]);
+}
+
+
+//main
+int main (int argc, char **argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv,numbers::invalid_unsigned_int);
+  try
+    {
+      deallog.depth_console(0);
+      CoupledCHACMechanicsProblem<problemDIM> problem;
+      problem.fields.push_back(Field<problemDIM>(SCALAR, PARABOLIC, "c"));
+      problem.fields.push_back(Field<problemDIM>(SCALAR, PARABOLIC, "n1"));
+      problem.fields.push_back(Field<problemDIM>(SCALAR, PARABOLIC, "n2"));
+      problem.fields.push_back(Field<problemDIM>(SCALAR, PARABOLIC, "n3"));
+      problem.fields.push_back(Field<problemDIM>(VECTOR,  ELLIPTIC, "u"));
+      problem.init(); 
+      problem.solve();
+    }
+  catch (std::exception &exc)
+    {
+      std::cerr << std::endl << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  catch (...)
+    {
+      std::cerr << std::endl << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+    }
+  
+  return 0;
+}
