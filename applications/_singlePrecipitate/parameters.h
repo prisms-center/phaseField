b98d1b1538178afe15a1aacd9a54b083db2a2059
Stephen DeWitt <stvdwtt@umich.edu>
2016-01-18 11:31:49 -0500

    changed some parameters while writing tests

--- applications/_singlePrecipitate/parameters.h
+++ applications/_singlePrecipitate/parameters.h
@@ -10,11 +10,11 @@
 #define subdivisionsX 1
 #define subdivisionsY 1
 #define subdivisionsZ 1
-#define refineFactor 7
-#define finiteElementDegree 1
+#define refineFactor 6
+#define finiteElementDegree 2
 
 //define time step parameters
-#define timeStep 3.0e-5 //5e-6 //1.67e-5
+#define timeStep 9.0e-5 //5e-6 //1.67e-5
 #define timeIncrements 10000 //200000
 #define timeFinal 100000000 //(timeStep*timeIncrements)
 #define skipImplicitSolves 10000000
@@ -42,11 +42,11 @@
 #define Mn3V 50.0
 
 
-//double Kn1[3][3]={{0.0150,0,0},{0,0.0188,0},{0,0,0.00571}};
+double Kn1[3][3]={{0.0150,0,0},{0,0.0188,0},{0,0,0.00571}};
 //double Kn1[3][3]={{0.0150,0,0},{0,0.0150,0},{0,0,0.0150}};
 //double Kn1[3][3]={{0.123,0,0},{0,0.123,0},{0,0,0.123}};
 //double Kn1[3][3]={{0.123,0,0},{0,0.0295,0},{0,0,0.123}};
-double Kn1[3][3]={{1.0,0,0},{0,0.5,0},{0,0,1.0}};
+//double Kn1[3][3]={{1.0,0,0},{0,0.5,0},{0,0,1.0}};
 double Kn2[3][3]={{0.123,0,0},{0,0.123,0},{0,0,0.123}};
 double Kn3[3][3]={{0.123,0,0},{0,0.123,0},{0,0,0.123}};
 
@@ -103,6 +103,6 @@ double sf3Strain[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
 #define x_denom 1.0
 #define y_denom 1.0
 #define z_denom 1.0
-#define initial_interface_coeff 1.0 //0.215
+#define initial_interface_coeff 0.215
 #define initial_radius 7.0
 #define avg_Nd 0.004
