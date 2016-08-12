11342d05325cb470c4d1cd8e62d9e877252c92cf
Stephen DeWitt <stvdwtt@umich.edu>
2016-08-11 21:32:25 -0400

    changed how boundaries are marked to be compatible with deal.II versions 8.3 and later. The code is no longer compatible with deal.II version 8.2.

--- src/models/coupled/generalized_model_functions.h
+++ src/models/coupled/generalized_model_functions.h
@@ -837,10 +837,10 @@ void generalizedProblem<dim>::markBoundaries(){
 		for (unsigned int face_number=0; face_number<GeometryInfo<dim>::faces_per_cell;++face_number){
 			for (unsigned int i=0; i<dim; i++){
 				if ( std::fabs(cell->face(face_number)->center()(i) - (0)) < 1e-12 ){
-					cell->face(face_number)->set_boundary_indicator (2*i);
+					cell->face(face_number)->set_boundary_id (2*i);
 				}
 				else if (std::fabs(cell->face(face_number)->center()(i) - (domain_size[i])) < 1e-12){
-					cell->face(face_number)->set_boundary_indicator (2*i+1);
+					cell->face(face_number)->set_boundary_id (2*i+1);
 				}
 
 			}
