68445a1e53f570fc1fd838ee7390b80684ca6e2b
Stephen DeWitt <stvdwtt@users.noreply.github.com>
2017-01-19 23:47:33 -0600

    Making dealii from source

--- test_ci.sh
+++ test_ci.sh
@@ -5,22 +5,9 @@
 #./deal.II
 #sudo apt-get install libdeal.ii-dev libdeal.ii-doc g++ cmake 
 
-PRG=$PWD/programs
-
-if [ ! -d $PRG ] 
-then
-    echo "create folder `$PRG`"
-    mkdir $PRG
-fi
-
-# astyle
-if [ ! -d $PRG/astyle ]
-then
-    echo "Downloading and installing astyle."
-    mkdir $PRG/astyle
-    wget http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_linux.tar.gz  > /dev/null
-    tar xfz astyle_2.04_linux.tar.gz -C $PRG > /dev/null
-    cd $PRG/astyle/build/gcc
-    make -j4 > /dev/null
-fi
-
+git clone https://github.com/dealii/dealii
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=. ../dealii
+make install
+make test
