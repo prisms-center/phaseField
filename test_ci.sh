8852669b5eb6d2ee059d1545a8c03d56da0d70ac
Stephen DeWitt <stvdwtt@users.noreply.github.com>
2017-01-19 23:36:33 -0600

    Tried taking the dealii ci script

--- test_ci.sh
+++ test_ci.sh
@@ -3,5 +3,24 @@
 #wget https://github.com/dealii/dealii/releases/download/v8.4.1/dealii-8.4.1.dmg
 #hdiutil attach dealii-8.4.1.dmg
 #./deal.II
-sudo apt-get install libdeal.ii-dev libdeal.ii-doc g++ cmake 
+#sudo apt-get install libdeal.ii-dev libdeal.ii-doc g++ cmake 
+
+PRG=$PWD/programs
+
+if [ ! -d $PRG ] 
+then
+    echo "create folder `$PRG`"
+    mkdir $PRG
+fi
+
+# astyle
+if [ ! -d $PRG/astyle ]
+then
+    echo "Downloading and installing astyle."
+    mkdir $PRG/astyle
+    wget http://downloads.sourceforge.net/project/astyle/astyle/astyle%202.04/astyle_2.04_linux.tar.gz  > /dev/null
+    tar xfz astyle_2.04_linux.tar.gz -C $PRG > /dev/null
+    cd $PRG/astyle/build/gcc
+    make -j4 > /dev/null
+fi
 
