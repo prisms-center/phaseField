6d1755527dbf1dd695f20700d78786dc9e916e5a
Stephen DeWitt <stvdwtt@users.noreply.github.com>
2017-01-20 07:55:28 -0600

    Update test_ci.sh

--- test_ci.sh
+++ test_ci.sh
@@ -6,9 +6,9 @@
 #sudo apt-get install libdeal.ii-dev libdeal.ii-doc g++ cmake 
 
 
-git clone https://github.com/dealii/dealii
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=. ../dealii
-make -j2 install
-make test
+#git clone https://github.com/dealii/dealii
+#mkdir build
+#cd build
+#cmake -DCMAKE_INSTALL_PREFIX=. ../dealii
+#make -j2 install
+#make test
