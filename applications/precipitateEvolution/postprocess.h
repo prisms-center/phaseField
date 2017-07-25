7261251110ef1c91b9724f7e9b72915537a21804
Stephen DeWitt <stvdwtt@umich.edu>
2017-07-25 13:08:55 -0400

    applied v2.0 update to the mechanics app

--- applications/precipitateEvolution/postprocess.h
+++ applications/precipitateEvolution/postprocess.h
@@ -1,41 +1,37 @@
-/*
- * postprocess.h
- *
- *  Created on: Mar 29, 2017
- *      Author: stephendewitt
- */
-
-#ifndef APPLICATIONS_ALLENCAHN_POSTPROCESS_H_
-#define APPLICATIONS_ALLENCAHN_POSTPROCESS_H_
 
 // =================================================================================
-// Define the variables for postprocessing
+// Set the attributes of the primary field variables
 // =================================================================================
+void variableAttributeLoader::loadPostProcessorVariableAttributes(){
+	// Variable 0
+	set_variable_name				(0,"mg_c");
+	set_variable_type				(0,SCALAR);
 
+	set_need_value_residual_term	(0,true);
+	set_need_gradient_residual_term	(0,false);
 
-// =================================================================================
-
-template <int dim>
-void postProcessedFields(const std::vector<modelVariable<dim> > & modelVariablesList,
-												std::vector<modelResidual<dim> > & modelResidualsList,
-												const dealii::Point<dim, dealii::VectorizedArray<double> > q_point_loc,
-												const list_of_CIJ<dim> material_moduli) {
-
-
+    set_output_integral         	(0,true);
 
 }
 
 // =================================================================================
+// Define the expressions for the post-processed fields
+// =================================================================================
 
-// Template instantiations
-template void postProcessedFields<2>(const std::vector<modelVariable<2> > & modelVariablesList,
-		std::vector<modelResidual<2> > & modelResidualsList,
-		const dealii::Point<2, dealii::VectorizedArray<double> > q_point_loc,
-		const list_of_CIJ<2> material_moduli);
+template <int dim,int degree>
+void customPDE<dim,degree>::postProcessedFields(const variableContainer<dim,degree,dealii::VectorizedArray<double> > & variable_list,
+				variableContainer<dim,degree,dealii::VectorizedArray<double> > & pp_variable_list,
+												const dealii::Point<dim, dealii::VectorizedArray<double> > q_point_loc) const {
 
-template void postProcessedFields<3>(const std::vector<modelVariable<3> > & modelVariablesList,
-		std::vector<modelResidual<3> > & modelResidualsList,
-		const dealii::Point<3, dealii::VectorizedArray<double> > q_point_loc,
-		const list_of_CIJ<3> material_moduli);
+// The order parameter and its derivatives (names here should match those in the macros above)
+scalargradType cx = variable_list.get_scalar_gradient(0);
 
-#endif /* APPLICATIONS_ALLENCAHN_POSTPROCESS_H_ */
+scalargradType pp_field;
+pp_field[0] = cx[0];
+pp_field[1] = cx[1];
+
+
+// Residuals for the equation to evolve the order parameter (names here should match those in the macros above)
+pp_variable_list.set_scalar_value_residual_term(0, std::sqrt(pp_field[0]*pp_field[0]+pp_field[1]*pp_field[1]));
+
+}
