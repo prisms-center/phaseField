b0f744732d8879caa46f9af066c7a7c155404cd0
Beck Andrews <wband@umich.edu>
2017-08-08 17:43:48 -0400

    1st commit for anisotropyFacet; it works w. macro.

---
+++ applications/anisotropyFacet/ICs_and_BCs.h
@@ -0,0 +1,95 @@
+
+// ===========================================================================
+// FUNCTIONS FOR INITIAL CONDITIONS
+// ===========================================================================
+
+template <int dim>
+double InitialCondition<dim>::value (const Point<dim> &p, const unsigned int component) const
+{
+	  double scalar_IC = 0;
+	  // --------------------------------------------------------------------------
+	  // ENTER THE INITIAL CONDITIONS HERE FOR SCALAR FIELDS
+	  // --------------------------------------------------------------------------
+	  // Enter the function describing conditions for the fields at point "p".
+	  // Use "if" statements to set the initial condition for each variable
+	  // according to its variable index.
+
+	  double r=0.0;
+
+	  if (index == 0){
+			r = 0.0;
+			for (unsigned int dir = 0; dir < dim; dir++){
+				r += (p[dir]-userInputs.domain_size[dir]/2.0)*(p[dir]-userInputs.domain_size[dir]/2.0);
+			}
+			r = std::sqrt(r);
+			double n = 0.5*(1.0-std::tanh((r-userInputs.domain_size[0]/4.0)/4.0));
+			scalar_IC = 0.082*16.0/(userInputs.domain_size[0]/4.0)+(3.0*n*n-2.0*n*n*n);
+
+	  }
+	  else if (index == 1) {
+			r = 0.0;
+			for (unsigned int dir = 0; dir < dim; dir++){
+			  r += (p[dir]-userInputs.domain_size[dir]/2.0)*(p[dir]-userInputs.domain_size[dir]/2.0);
+			}
+			r = std::sqrt(r);
+            scalar_IC = 0.5*(1.0-std::tanh((r-userInputs.domain_size[0]/4.0)/4.0));
+	  }
+	  else {
+		  scalar_IC = 0.0;
+	  }
+
+	  // --------------------------------------------------------------------------
+	  return scalar_IC;
+}
+
+template <int dim>
+void InitialConditionVec<dim>::vector_value (const Point<dim> &p,Vector<double> &vector_IC) const
+{
+	  // --------------------------------------------------------------------------
+	  // ENTER THE INITIAL CONDITIONS HERE FOR VECTOR FIELDS
+	  // --------------------------------------------------------------------------
+	  // Enter the function describing conditions for the fields at point "p".
+	  // Use "if" statements to set the initial condition for each variable
+	  // according to its variable index.
+
+
+	  // --------------------------------------------------------------------------
+}
+
+// ===========================================================================
+// FUNCTIONS FOR NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS
+// ===========================================================================
+
+template <int dim>
+double NonUniformDirichletBC<dim>::value (const dealii::Point<dim> &p, const unsigned int component) const
+{
+    double scalar_BC=0;
+    // --------------------------------------------------------------------------
+    // ENTER THE NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS HERE FOR SCALAR FIELDS
+    // --------------------------------------------------------------------------
+    // Enter the function describing conditions for the fields at point "p".
+    // Use "if" statements to set the boundary condition for each variable
+    // according to its variable index. This function can be left blank if there
+    // are no non-uniform Dirichlet boundary conditions.
+
+
+    // -------------------------------------------------------------------------
+    return scalar_BC;
+}
+
+template <int dim>
+void NonUniformDirichletBCVec<dim>::vector_value (const dealii::Point<dim> &p, dealii::Vector<double> &vector_BC) const
+{
+
+    // --------------------------------------------------------------------------
+    // ENTER THE NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS HERE FOR VECTOR FIELDS
+    // --------------------------------------------------------------------------
+    // Enter the function describing conditions for the fields at point "p".
+    // Use "if" statements to set the boundary condition for each variable
+    // according to its variable index. This function can be left blank if there
+    // are no non-uniform Dirichlet boundary conditions.
+
+
+    // -------------------------------------------------------------------------
+
+}
