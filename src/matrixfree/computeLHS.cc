392615a4d3e8fd2e44ddd06d28e5d3bf5b635373
Stephen DeWitt <stvdwtt@umich.edu>
2017-10-13 11:51:53 -0400

    speeding up vmult by a few percent by refactoring how src2 is initialized

--- src/matrixfree/computeLHS.cc
+++ src/matrixfree/computeLHS.cc
@@ -9,9 +9,7 @@ void MatrixFreePDE<dim,degree>::vmult (vectorType &dst, const vectorType &src) c
   computing_timer.enter_section("matrixFreePDE: computeLHS");
 
   //create temporary copy of src vector as src2, as vector src is marked const and cannot be changed
-  dealii::parallel::distributed::Vector<double> src2;
-  matrixFreeObject.initialize_dof_vector(src2,  currentFieldIndex);
-  src2=src;
+  dealii::parallel::distributed::Vector<double> src2(src);
 
   //call cell_loop
   dst=0.0;
@@ -26,6 +24,7 @@ void MatrixFreePDE<dim,degree>::vmult (vectorType &dst, const vectorType &src) c
 
   //end log
   computing_timer.exit_section("matrixFreePDE: computeLHS");
+
 }
 
 template <int dim, int degree>
