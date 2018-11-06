d9eb75e96d42afdc5cdf1104887b0fe25f8006d2
Stephen DeWitt <stvdwtt@umich.edu>
2018-11-06 15:59:54 -0500

    pruned some apps not ready for the public

--- applications/_MgY_dakota/visitlog.py
+++
@@ -1,4 +0,0 @@
-# Visit 2.11.0 log file
-ScriptVersion = "2.11.0"
-if ScriptVersion != Version():
-    print "This script is for VisIt %s. It may not work with version %s" % (ScriptVersion, Version())
