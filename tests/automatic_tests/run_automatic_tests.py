a96c635b6e925747a15f3f15638ddb71e65a321e
David Montiel <dmontiel@umich.edu>
2024-02-14 19:02:43 -0500

    Ran automatic tests

--- tests/automatic_tests/run_automatic_tests.py
+++ tests/automatic_tests/run_automatic_tests.py
@@ -28,7 +28,7 @@ def run_unit_tests():
 	subprocess.call(["cmake", "."],stdout=f,stderr=f)
 	subprocess.call(["make", "release"],stdout=f)
 	subprocess.call(["make"],stdout=f)
-	subprocess.call(["mpirun", "-n", "2", "./main"],stdout=f)
+	subprocess.call(["srun", "-n", "2", "./main"],stdout=f)
 	f.close()
 
 	result_file = open("unit_test_results.txt","r")
@@ -61,11 +61,11 @@ def run_simulation(run_name,dir_path):
 
 	# Compile and run
 	subprocess.call(["cmake", "."]) # print to the screen to prevent a timeout on Travis
-	subprocess.call(["make", "release","-j9"],stdout=f)
+	subprocess.call(["make", "release"],stdout=f)
 	print("Compiling complete, running the regression test...")
 	sys.stdout.flush()
 	start = time.time()
-	subprocess.call(["mpirun", "-n", "1", "./main"],stdout=f)
+	subprocess.call(["srun", "-n", "1", "./main"],stdout=f)
 	end = time.time()
 	f.close()
 
