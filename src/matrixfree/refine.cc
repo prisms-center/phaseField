9b39eae4192bfdadd29d0eaad982c6e84561f915
landinjm <nephrite@DESKTOP-DQBN823>
2024-06-01 16:30:55 -0400

    Removing old commented code

--- src/matrixfree/refine.cc
+++ src/matrixfree/refine.cc
@@ -56,184 +56,152 @@ template <int dim, int degree>
 void
 MatrixFreePDE<dim, degree>::adaptiveRefineCriterion()
 {
-  // Old code to implement a Kelly error estimator
-  // Kelly error estimation criterion
-  // estimate cell wise errors for mesh refinement
-  // #if hAdaptivity==true
-  // #ifdef adaptivityType
-  // #if adaptivityType=="KELLY"
-  //  Vector<float> estimated_error_per_cell
-  //  (triangulation.n_locally_owned_active_cells());
-  //  KellyErrorEstimator<dim>::estimate
-  //  (*dofHandlersSet_nonconst[refinementDOF],
-  //				      QGaussLobatto<dim-1>(degree+1),
-  //				      typename FunctionMap<dim>::type(),
-  //				      *solutionSet[refinementDOF],
-  //				      estimated_error_per_cell,
-  //				      ComponentMask(),
-  //				      0,
-  //				      1,
-  //				      triangulation.locally_owned_subdomain());
-  //  //flag cells for refinement
-  //  parallel::distributed::GridRefinement::refine_and_coarsen_fixed_fraction
-  //  (triangulation,
-  //									    estimated_error_per_cell,
-  //									    topRefineFraction,
-  //									    bottomCoarsenFraction);
-  // #endif
-  // #endif
-  // #endif
+  std::vector<std::vector<double>> valuesV;
+  std::vector<std::vector<double>> gradientsV;
 
-  {
-    std::vector<std::vector<double>> valuesV;
-    std::vector<std::vector<double>> gradientsV;
+  QGaussLobatto<dim> quadrature(degree + 1);
+  const unsigned int num_quad_points = quadrature.size();
 
-    QGaussLobatto<dim> quadrature(degree + 1);
-    const unsigned int num_quad_points = quadrature.size();
+  // Set the correct update flags
+  bool need_value    = false;
+  bool need_gradient = false;
+  for (unsigned int field_index = 0; field_index < userInputs.refinement_criteria.size();
+       field_index++)
+    {
+      if (userInputs.refinement_criteria[field_index].criterion_type == VALUE ||
+          userInputs.refinement_criteria[field_index].criterion_type ==
+            VALUE_AND_GRADIENT)
+        {
+          need_value = true;
+        }
+      else if (userInputs.refinement_criteria[field_index].criterion_type == GRADIENT ||
+               userInputs.refinement_criteria[field_index].criterion_type ==
+                 VALUE_AND_GRADIENT)
+        {
+          need_gradient = true;
+        }
+    }
+  dealii::UpdateFlags update_flags;
+  if (need_value && !need_gradient)
+    {
+      update_flags = update_values;
+    }
+  else if (!need_value && need_gradient)
+    {
+      update_flags = update_gradients;
+    }
+  else
+    {
+      update_flags = update_values | update_gradients;
+    }
 
-    // Set the correct update flags
-    bool need_value    = false;
-    bool need_gradient = false;
-    for (unsigned int field_index = 0;
-         field_index < userInputs.refinement_criteria.size();
-         field_index++)
-      {
-        if (userInputs.refinement_criteria[field_index].criterion_type == VALUE ||
-            userInputs.refinement_criteria[field_index].criterion_type ==
-              VALUE_AND_GRADIENT)
-          {
-            need_value = true;
-          }
-        else if (userInputs.refinement_criteria[field_index].criterion_type == GRADIENT ||
-                 userInputs.refinement_criteria[field_index].criterion_type ==
-                   VALUE_AND_GRADIENT)
-          {
-            need_gradient = true;
-          }
-      }
-    dealii::UpdateFlags update_flags;
-    if (need_value && !need_gradient)
-      {
-        update_flags = update_values;
-      }
-    else if (!need_value && need_gradient)
-      {
-        update_flags = update_gradients;
-      }
-    else
-      {
-        update_flags = update_values | update_gradients;
-      }
+  FEValues<dim> fe_values(*FESet[userInputs.refinement_criteria[0].variable_index],
+                          quadrature,
+                          update_flags);
 
-    FEValues<dim> fe_values(*FESet[userInputs.refinement_criteria[0].variable_index],
-                            quadrature,
-                            update_flags);
+  std::vector<double>                         values(num_quad_points);
+  std::vector<double>                         gradient_magnitudes(num_quad_points);
+  std::vector<dealii::Tensor<1, dim, double>> gradients(num_quad_points);
 
-    std::vector<double>                         values(num_quad_points);
-    std::vector<double>                         gradient_magnitudes(num_quad_points);
-    std::vector<dealii::Tensor<1, dim, double>> gradients(num_quad_points);
+  typename DoFHandler<dim>::active_cell_iterator
+    cell = dofHandlersSet_nonconst[userInputs.refinement_criteria[0].variable_index]
+             ->begin_active(),
+    endc =
+      dofHandlersSet_nonconst[userInputs.refinement_criteria[0].variable_index]->end();
 
-    typename DoFHandler<dim>::active_cell_iterator
-      cell = dofHandlersSet_nonconst[userInputs.refinement_criteria[0].variable_index]
-               ->begin_active(),
-      endc =
-        dofHandlersSet_nonconst[userInputs.refinement_criteria[0].variable_index]->end();
+  typename parallel::distributed::Triangulation<dim>::active_cell_iterator t_cell =
+    triangulation.begin_active();
 
-    typename parallel::distributed::Triangulation<dim>::active_cell_iterator t_cell =
-      triangulation.begin_active();
+  for (; cell != endc; ++cell)
+    {
+      if (cell->is_locally_owned())
+        {
+          fe_values.reinit(cell);
 
-    for (; cell != endc; ++cell)
-      {
-        if (cell->is_locally_owned())
-          {
-            fe_values.reinit(cell);
+          for (unsigned int field_index = 0;
+               field_index < userInputs.refinement_criteria.size();
+               field_index++)
+            {
+              if (need_value)
+                {
+                  fe_values.get_function_values(
+                    *solutionSet[userInputs.refinement_criteria[field_index]
+                                   .variable_index],
+                    values);
+                  valuesV.push_back(values);
+                }
+              if (need_gradient)
+                {
+                  fe_values.get_function_gradients(
+                    *solutionSet[userInputs.refinement_criteria[field_index]
+                                   .variable_index],
+                    gradients);
 
-            for (unsigned int field_index = 0;
-                 field_index < userInputs.refinement_criteria.size();
-                 field_index++)
-              {
-                if (need_value)
-                  {
-                    fe_values.get_function_values(
-                      *solutionSet[userInputs.refinement_criteria[field_index]
-                                     .variable_index],
-                      values);
-                    valuesV.push_back(values);
-                  }
-                if (need_gradient)
-                  {
-                    fe_values.get_function_gradients(
-                      *solutionSet[userInputs.refinement_criteria[field_index]
-                                     .variable_index],
-                      gradients);
+                  for (unsigned int q_point = 0; q_point < num_quad_points; ++q_point)
+                    {
+                      gradient_magnitudes.at(q_point) = gradients.at(q_point).norm();
+                    }
 
-                    for (unsigned int q_point = 0; q_point < num_quad_points; ++q_point)
-                      {
-                        gradient_magnitudes.at(q_point) = gradients.at(q_point).norm();
-                      }
+                  gradientsV.push_back(gradient_magnitudes);
+                }
+            }
 
-                    gradientsV.push_back(gradient_magnitudes);
-                  }
-              }
+          bool mark_refine = false;
 
-            bool mark_refine = false;
+          for (unsigned int q_point = 0; q_point < num_quad_points; ++q_point)
+            {
+              for (unsigned int field_index = 0;
+                   field_index < userInputs.refinement_criteria.size();
+                   field_index++)
+                {
+                  if (userInputs.refinement_criteria[field_index].criterion_type ==
+                        VALUE ||
+                      userInputs.refinement_criteria[field_index].criterion_type ==
+                        VALUE_AND_GRADIENT)
+                    {
+                      if ((valuesV[field_index][q_point] >
+                           userInputs.refinement_criteria[field_index]
+                             .value_lower_bound) &&
+                          (valuesV[field_index][q_point] <
+                           userInputs.refinement_criteria[field_index].value_upper_bound))
+                        {
+                          mark_refine = true;
+                          break;
+                        }
+                    }
+                  if (userInputs.refinement_criteria[field_index].criterion_type ==
+                        GRADIENT ||
+                      userInputs.refinement_criteria[field_index].criterion_type ==
+                        VALUE_AND_GRADIENT)
+                    {
+                      if (gradientsV[field_index][q_point] >
+                          userInputs.refinement_criteria[field_index]
+                            .gradient_lower_bound)
+                        {
+                          mark_refine = true;
+                          break;
+                        }
+                    }
+                }
+            }
 
-            for (unsigned int q_point = 0; q_point < num_quad_points; ++q_point)
-              {
-                for (unsigned int field_index = 0;
-                     field_index < userInputs.refinement_criteria.size();
-                     field_index++)
-                  {
-                    if (userInputs.refinement_criteria[field_index].criterion_type ==
-                          VALUE ||
-                        userInputs.refinement_criteria[field_index].criterion_type ==
-                          VALUE_AND_GRADIENT)
-                      {
-                        if ((valuesV[field_index][q_point] >
-                             userInputs.refinement_criteria[field_index]
-                               .value_lower_bound) &&
-                            (valuesV[field_index][q_point] <
-                             userInputs.refinement_criteria[field_index]
-                               .value_upper_bound))
-                          {
-                            mark_refine = true;
-                            break;
-                          }
-                      }
-                    if (userInputs.refinement_criteria[field_index].criterion_type ==
-                          GRADIENT ||
-                        userInputs.refinement_criteria[field_index].criterion_type ==
-                          VALUE_AND_GRADIENT)
-                      {
-                        if (gradientsV[field_index][q_point] >
-                            userInputs.refinement_criteria[field_index]
-                              .gradient_lower_bound)
-                          {
-                            mark_refine = true;
-                            break;
-                          }
-                      }
-                  }
-              }
+          valuesV.clear();
+          gradientsV.clear();
 
-            valuesV.clear();
-            gradientsV.clear();
+          // limit the maximal and minimal refinement depth of the mesh
+          unsigned int current_level = t_cell->level();
 
-            // limit the maximal and minimal refinement depth of the mesh
-            unsigned int current_level = t_cell->level();
-
-            if ((mark_refine && current_level < userInputs.max_refinement_level))
-              {
-                cell->set_refine_flag();
-              }
-            else if (!mark_refine && current_level > userInputs.min_refinement_level)
-              {
-                cell->set_coarsen_flag();
-              }
-          }
-        ++t_cell;
-      }
-  }
+          if ((mark_refine && current_level < userInputs.max_refinement_level))
+            {
+              cell->set_refine_flag();
+            }
+          else if (!mark_refine && current_level > userInputs.min_refinement_level)
+            {
+              cell->set_coarsen_flag();
+            }
+        }
+      ++t_cell;
+    }
 }
 
 // refine grid method
