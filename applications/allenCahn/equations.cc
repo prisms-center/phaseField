9cbb85a4b35a0158ed8d749a6f2eb564b595e010
ellery-hendrix <joyhen@umich.edu>
2024-09-13 16:19:45 -0400

    maybe_used for allenCahn

--- applications/allenCahn/equations.cc
+++ applications/allenCahn/equations.cc
@@ -41,8 +41,8 @@ variableAttributeLoader::loadVariableAttributes()
 template <int dim, int degree>
 void
 customPDE<dim, degree>::explicitEquationRHS(
-  variableContainer<dim, degree, dealii::VectorizedArray<double>> &variable_list,
-  dealii::Point<dim, dealii::VectorizedArray<double>>              q_point_loc) const
+  [[maybe_unused]] variableContainer<dim, degree, dealii::VectorizedArray<double>> &variable_list,
+  [[maybe_unused]] dealii::Point<dim, dealii::VectorizedArray<double>>              q_point_loc) const
 {
   // --- Getting the values and derivatives of the model variables ---
 
@@ -78,8 +78,8 @@ customPDE<dim, degree>::explicitEquationRHS(
 template <int dim, int degree>
 void
 customPDE<dim, degree>::nonExplicitEquationRHS(
-  variableContainer<dim, degree, dealii::VectorizedArray<double>> &variable_list,
-  dealii::Point<dim, dealii::VectorizedArray<double>>              q_point_loc) const
+  [[maybe_unused]] variableContainer<dim, degree, dealii::VectorizedArray<double>> &variable_list,
+  [[maybe_unused]] dealii::Point<dim, dealii::VectorizedArray<double>>              q_point_loc) const
 {}
 
 // =============================================================================================
@@ -100,6 +100,6 @@ customPDE<dim, degree>::nonExplicitEquationRHS(
 template <int dim, int degree>
 void
 customPDE<dim, degree>::equationLHS(
-  variableContainer<dim, degree, dealii::VectorizedArray<double>> &variable_list,
-  dealii::Point<dim, dealii::VectorizedArray<double>>              q_point_loc) const
+  [[maybe_unused]] variableContainer<dim, degree, dealii::VectorizedArray<double>> &variable_list,
+  [[maybe_unused]] dealii::Point<dim, dealii::VectorizedArray<double>>              q_point_loc) const
 {}
