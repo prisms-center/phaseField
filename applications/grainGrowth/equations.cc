87142242a915a8bc00f81f6ca4fd9c8509fc70be
landinjm <landinjm@umich.edu>
2024-09-15 23:56:16 -0400

    autoformatting

--- applications/grainGrowth/equations.cc
+++ applications/grainGrowth/equations.cc
@@ -48,8 +48,9 @@ variableAttributeLoader::loadVariableAttributes()
 template <int dim, int degree>
 void
 customPDE<dim, degree>::explicitEquationRHS(
-  [[maybe_unused]] variableContainer<dim, degree, dealii::VectorizedArray<double>> &variable_list,
-  [[maybe_unused]] dealii::Point<dim, dealii::VectorizedArray<double>>              q_point_loc) const
+  [[maybe_unused]] variableContainer<dim, degree, dealii::VectorizedArray<double>>
+                                                                      &variable_list,
+  [[maybe_unused]] dealii::Point<dim, dealii::VectorizedArray<double>> q_point_loc) const
 {
   // --- Getting the values and derivatives of the model variables ---
 
@@ -111,8 +112,9 @@ customPDE<dim, degree>::explicitEquationRHS(
 template <int dim, int degree>
 void
 customPDE<dim, degree>::nonExplicitEquationRHS(
-  [[maybe_unused]] variableContainer<dim, degree, dealii::VectorizedArray<double>> &variable_list,
-  [[maybe_unused]] dealii::Point<dim, dealii::VectorizedArray<double>>              q_point_loc) const
+  [[maybe_unused]] variableContainer<dim, degree, dealii::VectorizedArray<double>>
+                                                                      &variable_list,
+  [[maybe_unused]] dealii::Point<dim, dealii::VectorizedArray<double>> q_point_loc) const
 {}
 
 // =============================================================================================
@@ -133,6 +135,7 @@ customPDE<dim, degree>::nonExplicitEquationRHS(
 template <int dim, int degree>
 void
 customPDE<dim, degree>::equationLHS(
-  [[maybe_unused]] variableContainer<dim, degree, dealii::VectorizedArray<double>> &variable_list,
-  [[maybe_unused]] dealii::Point<dim, dealii::VectorizedArray<double>>              q_point_loc) const
+  [[maybe_unused]] variableContainer<dim, degree, dealii::VectorizedArray<double>>
+                                                                      &variable_list,
+  [[maybe_unused]] dealii::Point<dim, dealii::VectorizedArray<double>> q_point_loc) const
 {}
