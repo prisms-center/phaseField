7ac1b6b698290e586c7ed51d1210e6a082d6d11f
Jason Landini <landinjm@umich.edu>
2024-09-26 10:39:22 -0400

    autoformat

--- tests/automatic_tests/run_automatic_tests.py
+++ tests/automatic_tests/run_automatic_tests.py
@@ -7,171 +7,178 @@ import datetime
 import time
 import sys
 
+
 # ----------------------------------------------------------------------------------------
 # Function that compiles and runs the unit tests.
 # ----------------------------------------------------------------------------------------
 def run_unit_tests():
-	# Open file where output is redirected to
-	if os.path.exists("output.txt") == True:
-		os.remove("output.txt")
-	f = open("output.txt",'w+')
+    # Open file where output is redirected to
+    if os.path.exists("output.txt") == True:
+        os.remove("output.txt")
+    f = open("output.txt", "w+")
 
-	# Remove old files
-	if os.path.exists("main") == True:
-		os.remove("main")
-	if os.path.exists("CMakeCache.txt") == True:
-		os.remove("CMakeCache.txt")
-	if os.path.exists("unit_test_results.txt") == True:
-		os.remove("unit_test_results.txt")
+    # Remove old files
+    if os.path.exists("main") == True:
+        os.remove("main")
+    if os.path.exists("CMakeCache.txt") == True:
+        os.remove("CMakeCache.txt")
+    if os.path.exists("unit_test_results.txt") == True:
+        os.remove("unit_test_results.txt")
 
-	# Compile and run
-	subprocess.call(["cmake", "."],stdout=f,stderr=f)
-	subprocess.call(["make", "release"],stdout=f)
-	subprocess.call(["make"],stdout=f)
-	subprocess.call(["mpirun", "-n", "1", "./main"],stdout=f)
-	f.close()
+    # Compile and run
+    subprocess.call(["cmake", "."], stdout=f, stderr=f)
+    subprocess.call(["make", "release"], stdout=f)
+    subprocess.call(["make"], stdout=f)
+    subprocess.call(["mpirun", "-n", "1", "./main"], stdout=f)
+    f.close()
 
-	result_file = open("unit_test_results.txt","r")
-	test_results = result_file.read().splitlines()
-	result_file.close()
+    result_file = open("unit_test_results.txt", "r")
+    test_results = result_file.read().splitlines()
+    result_file.close()
 
-	return test_results
+    return test_results
 
 
 # ----------------------------------------------------------------------------------------
 # Function that compiles the PRISMS-PF code and runs the executable.
 # ----------------------------------------------------------------------------------------
-def run_simulation(run_name,dir_path):
-	# Delete any pre-existing executables or results
-	if os.path.exists(run_name) == True:
-		shutil.rmtree(run_name)
+def run_simulation(run_name, dir_path):
+    # Delete any pre-existing executables or results
+    if os.path.exists(run_name) == True:
+        shutil.rmtree(run_name)
 
-	# Open file where output is redirected to
-	if os.path.exists("output.txt") == True:
-		os.remove("output.txt")
-	f = open("output.txt",'w+')
+    # Open file where output is redirected to
+    if os.path.exists("output.txt") == True:
+        os.remove("output.txt")
+    f = open("output.txt", "w+")
 
-	# Remove old files
-	if os.path.exists("main") == True:
-		os.remove("main")
-	if os.path.exists("CMakeCache.txt") == True:
-		os.remove("CMakeCache.txt")
+    # Remove old files
+    if os.path.exists("main") == True:
+        os.remove("main")
+    if os.path.exists("CMakeCache.txt") == True:
+        os.remove("CMakeCache.txt")
 
-	subprocess.call(["rm", "*vtu"],stdout=f,stderr=f)
+    subprocess.call(["rm", "*vtu"], stdout=f, stderr=f)
 
-	# Compile and run
-	subprocess.call(["cmake", "."]) # print to the screen to prevent a timeout on Travis
-	subprocess.call(["make", "release"],stdout=f)
-	print("Compiling complete, running the regression test...")
-	sys.stdout.flush()
-	start = time.time()
-	subprocess.call(["mpirun", "-n", "1", "./main"],stdout=f)
-	end = time.time()
-	f.close()
+    # Compile and run
+    subprocess.call(
+        ["cmake", "."]
+    )  # print to the screen to prevent a timeout on Travis
+    subprocess.call(["make", "release"], stdout=f)
+    print("Compiling complete, running the regression test...")
+    sys.stdout.flush()
+    start = time.time()
+    subprocess.call(["mpirun", "-n", "1", "./main"], stdout=f)
+    end = time.time()
+    f.close()
 
-	# Group the files
-	subprocess.call(["mkdir",run_name])
-	for output_files in glob.glob('*vtu'):
-		shutil.move(output_files,run_name)
-	if os.path.exists("integratedFields.txt") == True:
-		shutil.move("integratedFields.txt",run_name)
+    # Group the files
+    subprocess.call(["mkdir", run_name])
+    for output_files in glob.glob("*vtu"):
+        shutil.move(output_files, run_name)
+    if os.path.exists("integratedFields.txt") == True:
+        shutil.move("integratedFields.txt", run_name)
 
-	test_time = end-start
-	return test_time
+    test_time = end - start
+    return test_time
+
 
 # ----------------------------------------------------------------------------------------
 # Function that runs a regression test
 # ----------------------------------------------------------------------------------------
-def run_regression_test(applicationName,getNewGoldStandard,dir_path):
+def run_regression_test(applicationName, getNewGoldStandard, dir_path):
 
-	if (getNewGoldStandard == False):
-		testName = "test_"+applicationName
+    if getNewGoldStandard == False:
+        testName = "test_" + applicationName
 
-	else:
-		testName = "gold_"+applicationName
+    else:
+        testName = "gold_" + applicationName
 
-	if os.path.exists(testName) == True:
-		shutil.rmtree(testName)
+    if os.path.exists(testName) == True:
+        shutil.rmtree(testName)
 
-	# Move to the application directory
-	r_test_dir = dir_path
-	os.chdir("../../applications/"+applicationName)
+    # Move to the application directory
+    r_test_dir = dir_path
+    os.chdir("../../applications/" + applicationName)
 
-	# Run the simulation and move the results to the test directory
-	test_time = run_simulation(testName,dir_path)
+    # Run the simulation and move the results to the test directory
+    test_time = run_simulation(testName, dir_path)
 
-	shutil.move(testName,r_test_dir)
+    shutil.move(testName, r_test_dir)
 
-	# Compare the result against the gold standard, if it exists
-	os.chdir(r_test_dir)
+    # Compare the result against the gold standard, if it exists
+    os.chdir(r_test_dir)
 
-	if (getNewGoldStandard == False):
-		# Read the gold standard free energies
-		os.chdir("gold_"+applicationName)
-		gold_standard_file = open("integratedFields.txt","r")
-		gold_energy = gold_standard_file.readlines()
-		gold_standard_file.close()
+    if getNewGoldStandard == False:
+        # Read the gold standard free energies
+        os.chdir("gold_" + applicationName)
+        gold_standard_file = open("integratedFields.txt", "r")
+        gold_energy = gold_standard_file.readlines()
+        gold_standard_file.close()
 
-		last_energy_index = len(gold_energy)-1
-		split_last_line = gold_energy[-1].split()
-		for index, entry in enumerate(split_last_line):
-			if entry == "f_tot":
-				gold_last_energy = split_last_line[index+1]
+        last_energy_index = len(gold_energy) - 1
+        split_last_line = gold_energy[-1].split()
+        for index, entry in enumerate(split_last_line):
+            if entry == "f_tot":
+                gold_last_energy = split_last_line[index + 1]
 
-		# Read the test free energies
-		os.chdir("../"+testName)
-		test_file = open("integratedFields.txt","r")
-		test_energy = test_file.readlines()
-		test_file.close()
+        # Read the test free energies
+        os.chdir("../" + testName)
+        test_file = open("integratedFields.txt", "r")
+        test_energy = test_file.readlines()
+        test_file.close()
 
-		last_energy_index = len(test_energy)-1
-		split_last_line = test_energy[-1].split()
-		for index, entry in enumerate(split_last_line):
-			if entry == "f_tot":
-				last_energy = split_last_line[index+1]
+        last_energy_index = len(test_energy) - 1
+        split_last_line = test_energy[-1].split()
+        for index, entry in enumerate(split_last_line):
+            if entry == "f_tot":
+                last_energy = split_last_line[index + 1]
 
-		rel_diff = (float(gold_last_energy)-float(last_energy))/float(gold_last_energy)
-		rel_diff = abs(rel_diff)
+        rel_diff = (float(gold_last_energy) - float(last_energy)) / float(
+            gold_last_energy
+        )
+        rel_diff = abs(rel_diff)
 
-		if (rel_diff < 1.0e-9):
-			test_passed = True
-		else:
-			test_passed = False
+        if rel_diff < 1.0e-9:
+            test_passed = True
+        else:
+            test_passed = False
 
-	else:
-		test_passed = True
+    else:
+        test_passed = True
 
-	# Print the results to the screen
-	print("Regression Test: ", applicationName)
+    # Print the results to the screen
+    print("Regression Test: ", applicationName)
 
-	if test_passed:
-		if getNewGoldStandard == False:
-			print("Result: Pass")
-		else:
-			print("Result: New Gold Standard")
-	else:
-		print("Result: Fail")
+    if test_passed:
+        if getNewGoldStandard == False:
+            print("Result: Pass")
+        else:
+            print("Result: New Gold Standard")
+    else:
+        print("Result: Fail")
 
-	print("Time taken:", test_time)
+    print("Time taken:", test_time)
 
-	sys.stdout.flush()
+    sys.stdout.flush()
 
-	# Write the results to a file
-	os.chdir(r_test_dir)
-	text_file = open("test_results.txt","a")
-	now = datetime.datetime.now()
-	text_file.write("Application: " + applicationName +" \n")
-	if test_passed:
-		if getNewGoldStandard == False:
-			text_file.write("Result: Pass \n")
-		else:
-			text_file.write("Result: New Gold Standard \n")
-	else:
-		text_file.write("Result: Fail \n")
-	text_file.write("Time: "+str(test_time)+" \n \n")
-	text_file.close()
+    # Write the results to a file
+    os.chdir(r_test_dir)
+    text_file = open("test_results.txt", "a")
+    now = datetime.datetime.now()
+    text_file.write("Application: " + applicationName + " \n")
+    if test_passed:
+        if getNewGoldStandard == False:
+            text_file.write("Result: Pass \n")
+        else:
+            text_file.write("Result: New Gold Standard \n")
+    else:
+        text_file.write("Result: Fail \n")
+    text_file.write("Time: " + str(test_time) + " \n \n")
+    text_file.close()
 
-	return (test_passed,test_time)
+    return (test_passed, test_time)
+
 
 # ----------------------------------------------------------------------------------------
 # Test Script
@@ -181,7 +188,7 @@ def run_regression_test(applicationName,getNewGoldStandard,dir_path):
 dir_path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(dir_path)
 
-text_file = open("test_results.txt","a")
+text_file = open("test_results.txt", "a")
 now = datetime.datetime.now()
 
 # ------------------------------------
@@ -195,14 +202,18 @@ unit_tests_passed = unit_test_results[0]
 unit_test_counter = unit_test_results[1]
 
 print()
-print("Unit Tests Passed: "+str(unit_tests_passed)+"/"+str(unit_test_counter)+"\n")
+print(
+    "Unit Tests Passed: " + str(unit_tests_passed) + "/" + str(unit_test_counter) + "\n"
+)
 
 sys.stdout.flush()
 
 text_file.write("--------------------------------------------------------- \n")
 text_file.write("Unit test on " + now.strftime("%Y-%m-%d %H:%M") + "\n")
 text_file.write("--------------------------------------------------------- \n")
-text_file.write("Unit Tests Passed: "+str(unit_tests_passed)+"/"+str(unit_test_counter)+"\n")
+text_file.write(
+    "Unit Tests Passed: " + str(unit_tests_passed) + "/" + str(unit_test_counter) + "\n"
+)
 
 os.chdir(dir_path)
 
@@ -218,35 +229,56 @@ text_file.write("Regression test on " + now.strftime("%Y-%m-%d %H:%M") + "\n")
 text_file.write("--------------------------------------------------------- \n")
 text_file.close()
 
-#applicationList = ["allenCahn","cahnHilliard","cahnHilliard","CHAC_anisotropy","CHAC_anisotropyRegularized","coupledCahnHilliardAllenCahn","mechanics","precipitateEvolution"]
-#getNewGoldStandardList = [False, False, False, False, False, False, False, False]
+# applicationList = ["allenCahn","cahnHilliard","cahnHilliard","CHAC_anisotropy","CHAC_anisotropyRegularized","coupledCahnHilliardAllenCahn","mechanics","precipitateEvolution"]
+# getNewGoldStandardList = [False, False, False, False, False, False, False, False]
 
 # Shorter list of applications so that it completes on Travis
-applicationList = ["allenCahn","cahnHilliard","CHAC_anisotropyRegularized","coupledCahnHilliardAllenCahn","precipitateEvolution"]
+applicationList = [
+    "allenCahn",
+    "cahnHilliard",
+    "CHAC_anisotropyRegularized",
+    "coupledCahnHilliardAllenCahn",
+    "precipitateEvolution",
+]
 getNewGoldStandardList = [False, False, False, False, False]
-#applicationList = ["allenCahn"]
-#getNewGoldStandardList = [False]
-
+# applicationList = ["allenCahn"]
+# getNewGoldStandardList = [False]
 
 
 for applicationName in applicationList:
-	test_result = run_regression_test(applicationName,getNewGoldStandardList[regression_test_counter],dir_path)
+    test_result = run_regression_test(
+        applicationName, getNewGoldStandardList[regression_test_counter], dir_path
+    )
 
-	regression_test_counter += 1
-	regression_tests_passed += int(test_result[0])
+    regression_test_counter += 1
+    regression_tests_passed += int(test_result[0])
 
 print()
-print("Regression Tests Passed: "+str(regression_tests_passed)+"/"+str(regression_test_counter)+"\n")
+print(
+    "Regression Tests Passed: "
+    + str(regression_tests_passed)
+    + "/"
+    + str(regression_test_counter)
+    + "\n"
+)
 
 
 # Output the overall test results
-text_file = open("test_results.txt","a")
-text_file.write("Tests Passed: "+str(regression_tests_passed)+"/"+str(regression_test_counter)+"\n")
+text_file = open("test_results.txt", "a")
+text_file.write(
+    "Tests Passed: "
+    + str(regression_tests_passed)
+    + "/"
+    + str(regression_test_counter)
+    + "\n"
+)
 text_file.write("--------------------------------------------------------- \n")
 text_file.close()
 
 # Set exit code (passed to Travis CI)
-if ((regression_tests_passed < regression_test_counter) or (unit_tests_passed < unit_test_counter)):
-	sys.exit(1)
+if (regression_tests_passed < regression_test_counter) or (
+    unit_tests_passed < unit_test_counter
+):
+    sys.exit(1)
 else:
-	sys.exit(0)
+    sys.exit(0)
