01ebc5751a2640982f882c55624e478c15e255e3
Jason Landini <landinjm@umich.edu>
2024-10-24 10:29:57 -0400

    Revert "Dirichlet constraints can be distributed here"
    This reverts commit b0f8edb0d0a43c43058c4bb4d1438a3d4ea85ab9.

--- src/matrixfree/computeLHS.cc
+++ src/matrixfree/computeLHS.cc
@@ -35,8 +35,18 @@ MatrixFreePDE<dim, degree>::vmult(vectorType &dst, const vectorType &src) const
                                  true);
     }
 
-  // Account for Dirichlet BC's
-  constraintsDirichletSet[currentFieldIndex]->distribute(dst);
+  // Account for Dirichlet BC's (essentially copy dirichlet DOF values present in src to
+  // dst, although it is unclear why the constraints can't just be distributed here)
+  for (std::map<types::global_dof_index, double>::const_iterator it =
+         valuesDirichletSet[currentFieldIndex]->begin();
+       it != valuesDirichletSet[currentFieldIndex]->end();
+       ++it)
+    {
+      if (dst.in_local_range(it->first))
+        {
+          dst(it->first) = src(it->first); //*jacobianDiagonal(it->first);
+        }
+    }
 
   // end log
   computing_timer.leave_subsection("matrixFreePDE: computeLHS");
