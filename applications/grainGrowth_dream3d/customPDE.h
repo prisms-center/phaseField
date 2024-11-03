3ab65f4e87a135578c3e4b3c1701993bfcc554de
supriyoumich <supriyc@umich.edu>
2024-11-02 21:27:07 -0400

    Add files via upload

--- applications/grainGrowth_dream3d/customPDE.h
+++ applications/grainGrowth_dream3d/customPDE.h
@@ -1,60 +1,94 @@
-#include "../../include/matrixFreePDE.h"
+#include "matrixFreePDE.h"
+
+using namespace dealii;
 
 template <int dim, int degree>
-class customPDE: public MatrixFreePDE<dim,degree>
+class customPDE : public MatrixFreePDE<dim, degree>
 {
 public:
-    // Constructor
-    customPDE(userInputParameters<dim> _userInputs): MatrixFreePDE<dim,degree>(_userInputs) , userInputs(_userInputs) {};
+  // Constructor
+  customPDE(userInputParameters<dim> _userInputs)
+    : MatrixFreePDE<dim, degree>(_userInputs)
+    , userInputs(_userInputs) {};
 
-    // Function to set the initial conditions (in ICs_and_BCs.h)
-    void setInitialCondition(const dealii::Point<dim> &p, const unsigned int index, double & scalar_IC, dealii::Vector<double> & vector_IC);
+  // Function to set the initial conditions (in ICs_and_BCs.h)
+  void
+  setInitialCondition([[maybe_unused]] const Point<dim>  &p,
+                      [[maybe_unused]] const unsigned int index,
+                      [[maybe_unused]] double            &scalar_IC,
+                      [[maybe_unused]] Vector<double>    &vector_IC) override;
 
-    // Function to set the non-uniform Dirichlet boundary conditions (in ICs_and_BCs.h)
-    void setNonUniformDirichletBCs(const dealii::Point<dim> &p, const unsigned int index, const unsigned int direction, const double time, double & scalar_BC, dealii::Vector<double> & vector_BC);
+  // Function to set the non-uniform Dirichlet boundary conditions (in
+  // ICs_and_BCs.h)
+  void
+  setNonUniformDirichletBCs([[maybe_unused]] const Point<dim>  &p,
+                            [[maybe_unused]] const unsigned int index,
+                            [[maybe_unused]] const unsigned int direction,
+                            [[maybe_unused]] const double       time,
+                            [[maybe_unused]] double            &scalar_BC,
+                            [[maybe_unused]] Vector<double>    &vector_BC) override;
 
 private:
-	#include "../../include/typeDefs.h"
+#include "typeDefs.h"
 
-	const userInputParameters<dim> userInputs;
+  const userInputParameters<dim> userInputs;
 
-	// Function to set the RHS of the governing equations for explicit time dependent equations (in equations.h)
-    void explicitEquationRHS(variableContainer<dim,degree,dealii::VectorizedArray<double> > & variable_list,
-					 dealii::Point<dim, dealii::VectorizedArray<double> > q_point_loc) const;
+  // Function to set the RHS of the governing equations for explicit time
+  // dependent equations (in equations.h)
+  void
+  explicitEquationRHS(
+    [[maybe_unused]] variableContainer<dim, degree, VectorizedArray<double>>
+                                                              &variable_list,
+    [[maybe_unused]] const Point<dim, VectorizedArray<double>> q_point_loc,
+    [[maybe_unused]] const VectorizedArray<double> element_volume) const override;
 
-    // Function to set the RHS of the governing equations for all other equations (in equations.h)
-    void nonExplicitEquationRHS(variableContainer<dim,degree,dealii::VectorizedArray<double> > & variable_list,
-					 dealii::Point<dim, dealii::VectorizedArray<double> > q_point_loc) const;
+  // Function to set the RHS of the governing equations for all other equations
+  // (in equations.h)
+  void
+  nonExplicitEquationRHS(
+    [[maybe_unused]] variableContainer<dim, degree, VectorizedArray<double>>
+                                                              &variable_list,
+    [[maybe_unused]] const Point<dim, VectorizedArray<double>> q_point_loc,
+    [[maybe_unused]] const VectorizedArray<double> element_volume) const override;
 
-	// Function to set the LHS of the governing equations (in equations.h)
-	void equationLHS(variableContainer<dim,degree,dealii::VectorizedArray<double> > & variable_list,
-					 dealii::Point<dim, dealii::VectorizedArray<double> > q_point_loc) const;
+  // Function to set the LHS of the governing equations (in equations.h)
+  void
+  equationLHS(
+    [[maybe_unused]] variableContainer<dim, degree, VectorizedArray<double>>
+                                                              &variable_list,
+    [[maybe_unused]] const Point<dim, VectorizedArray<double>> q_point_loc,
+    [[maybe_unused]] const VectorizedArray<double> element_volume) const override;
 
-	// Function to set postprocessing expressions (in postprocess.h)
-	#ifdef POSTPROCESS_FILE_EXISTS
-	void postProcessedFields(const variableContainer<dim,degree,dealii::VectorizedArray<double> > & variable_list,
-					variableContainer<dim,degree,dealii::VectorizedArray<double> > & pp_variable_list,
-					const dealii::Point<dim, dealii::VectorizedArray<double> > q_point_loc) const;
-	#endif
+// Function to set postprocessing expressions (in postprocess.h)
+#ifdef POSTPROCESS_FILE_EXISTS
+  void
+  postProcessedFields(
+    [[maybe_unused]] const variableContainer<dim, degree, VectorizedArray<double>>
+      &variable_list,
+    [[maybe_unused]] variableContainer<dim, degree, VectorizedArray<double>>
+                                                              &pp_variable_list,
+    [[maybe_unused]] const Point<dim, VectorizedArray<double>> q_point_loc,
+    [[maybe_unused]] const VectorizedArray<double> element_volume) const override;
+#endif
 
-	// Function to set the nucleation probability (in nucleation.h)
-	#ifdef NUCLEATION_FILE_EXISTS
-	double getNucleationProbability(variableValueContainer variable_value, double dV) const;
-	#endif
+// Function to set the nucleation probability (in nucleation.h)
+#ifdef NUCLEATION_FILE_EXISTS
+  double
+  getNucleationProbability([[maybe_unused]] variableValueContainer variable_value,
+                           [[maybe_unused]] double                 dV) const override;
+#endif
 
-	// ================================================================
-	// Methods specific to this subclass
-	// ================================================================
+  // ================================================================
+  // Methods specific to this subclass
+  // ================================================================
 
+  // ================================================================
+  // Model constants specific to this subclass
+  // ================================================================
 
-	// ================================================================
-	// Model constants specific to this subclass
-	// ================================================================
+  double MnV   = userInputs.get_model_constant_double("MnV");
+  double KnV   = userInputs.get_model_constant_double("KnV");
+  double alpha = userInputs.get_model_constant_double("alpha");
 
-	double MnV = userInputs.get_model_constant_double("MnV");
-	double KnV = userInputs.get_model_constant_double("KnV");
-	double alpha = userInputs.get_model_constant_double("alpha");
-
-	// ================================================================
-
+  // ================================================================
 };
