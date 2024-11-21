484db725e43558c4b76d390856d8b0f68302ef57
Jason Landini <landinjm@umich.edu>
2024-11-21 13:02:18 -0500

    updating field unit tests and field.h

--- include/fields.h
+++ include/fields.h
@@ -1,15 +1,26 @@
-// fields class
 #ifndef FIELDS_H
 #define FIELDS_H
+
 #include <deal.II/base/conditional_ostream.h>
 
-#include "userInputParameters.h"
+#include "varTypeEnums.h"
 
+#include <stdexcept>
+#include <string>
+#include <utility>
+
+/**
+ * \brief Field class that handles attributes of each field.
+ */
 template <int dim>
 class Field
 {
 public:
+  /**
+   * \brief Constructor.
+   */
   Field(fieldType _type, PDEType _pdetype, std::string _name);
+
   fieldType    type;
   PDEType      pdetype;
   std::string  name;
@@ -21,52 +32,110 @@ public:
   bool         hasNeumannBCs;
 
 private:
+  /**
+   * \brief Total global fields.
+   */
   static unsigned int fieldCount;
+
+  /**
+   * \brief Total global indices
+   */
   static unsigned int indexCount;
+
+  /**
+   * \brief Validate the PDEtype and fieldType passed into the constructor.
+   */
+  void
+  validate_enum_types();
+
+  /**
+   * \brief Initialize the number of components based on whether the field is a scalar or
+   * vector.
+   */
+  void
+  init_components();
+
+  /**
+   * \brief Initialize the boundary condition conditionals to false.
+   */
+  void
+  init_BCs();
 };
 
 // initialize static variables
 template <int dim>
 unsigned int Field<dim>::fieldCount = 0;
+
 template <int dim>
 unsigned int Field<dim>::indexCount = 0;
 
-// constructor
 template <int dim>
 Field<dim>::Field(fieldType _type, PDEType _pdetype, std::string _name)
   : type(_type)
   , pdetype(_pdetype)
-  , name(_name)
+  , name(std::move(_name))
 {
-  // increment field count as new field is being created
-  index = fieldCount;
-  fieldCount++;
+  validate_enum_types();
+
+  // Set the index to the current field count and increment field count as new field is
+  // being created
+  index      = fieldCount++;
   startIndex = indexCount;
 
-  // assign index to this field
+  init_components();
+
+  init_BCs();
+}
+
+template <int dim>
+void
+Field<dim>::validate_enum_types()
+{
+  // Validate fieldType
+  switch (type)
+    {
+      case fieldType::SCALAR:
+      case fieldType::VECTOR:
+        break;
+      default:
+        throw std::invalid_argument("Unknown field type in Field constructor.");
+    }
+
+  switch (pdetype)
+    {
+      case PDEType::EXPLICIT_TIME_DEPENDENT:
+      case PDEType::AUXILIARY:
+      case PDEType::IMPLICIT_TIME_DEPENDENT:
+      case PDEType::TIME_INDEPENDENT:
+        break;
+      default:
+        throw std::invalid_argument("Unknown PDE type in Field constructor.");
+    }
+}
+
+template <int dim>
+void
+Field<dim>::init_components()
+{
   switch (type)
     {
       case SCALAR:
-        {
-          // increment index count by one
-          indexCount += 1;
-          numComponents = 1;
-          break;
-        }
+        indexCount += 1;
+        numComponents = 1;
+        break;
       case VECTOR:
-        {
-          // increment index count by dim
-          indexCount += dim;
-          numComponents = dim;
-          break;
-        }
+        indexCount += dim;
+        numComponents = dim;
+        break;
       default:
-        {
-          std::cout << "fields.h: unknown field type\n";
-          exit(-1);
-        }
+        break;
     }
+}
 
+template <int dim>
+void
+Field<dim>::init_BCs()
+{
   // Default assignment of BCs
   hasDirichletBCs           = false;
   hasnonuniformDirichletBCs = false;
--- tests/core/Field.cc
+++ tests/core/Field.cc
@@ -1,7 +1,7 @@
 #include "../../contrib/catch/catch.hpp"
 #include "../../include/fields.h"
 
-TEST_CASE("Field declaration in 1D")
+TEST_CASE("Field declarations")
 {
   const std::string field_name = "phi";
 
@@ -9,62 +9,141 @@ TEST_CASE("Field declaration in 1D")
   {
     SECTION("EXPLICIT_TIME_DEPENDENT")
     {
-      Field<1> field(SCALAR, EXPLICIT_TIME_DEPENDENT, field_name);
+      Field<1> field_1d(SCALAR, EXPLICIT_TIME_DEPENDENT, field_name);
+      Field<2> field_2d(SCALAR, EXPLICIT_TIME_DEPENDENT, field_name);
+      Field<3> field_3d(SCALAR, EXPLICIT_TIME_DEPENDENT, field_name);
 
-      REQUIRE(field.numComponents == 1);
-      REQUIRE(field.index == 0);
+      REQUIRE(field_1d.numComponents == 1);
+      REQUIRE(field_1d.index == 0);
+
+      REQUIRE(field_2d.numComponents == 1);
+      REQUIRE(field_2d.index == 0);
+
+      REQUIRE(field_3d.numComponents == 1);
+      REQUIRE(field_3d.index == 0);
     }
     SECTION("IMPLICIT_TIME_DEPENDENT")
     {
-      Field<1> field(SCALAR, IMPLICIT_TIME_DEPENDENT, field_name);
+      Field<1> field_1d(SCALAR, IMPLICIT_TIME_DEPENDENT, field_name);
+      Field<2> field_2d(SCALAR, IMPLICIT_TIME_DEPENDENT, field_name);
+      Field<3> field_3d(SCALAR, IMPLICIT_TIME_DEPENDENT, field_name);
 
-      REQUIRE(field.numComponents == 1);
-      REQUIRE(field.index == 1);
+      REQUIRE(field_1d.numComponents == 1);
+      REQUIRE(field_1d.index == 1);
+
+      REQUIRE(field_2d.numComponents == 1);
+      REQUIRE(field_2d.index == 1);
+
+      REQUIRE(field_3d.numComponents == 1);
+      REQUIRE(field_3d.index == 1);
     }
     SECTION("TIME_INDEPENDENT")
     {
-      Field<1> field(SCALAR, TIME_INDEPENDENT, field_name);
+      Field<1> field_1d(SCALAR, TIME_INDEPENDENT, field_name);
+      Field<2> field_2d(SCALAR, TIME_INDEPENDENT, field_name);
+      Field<3> field_3d(SCALAR, TIME_INDEPENDENT, field_name);
 
-      REQUIRE(field.numComponents == 1);
-      REQUIRE(field.index == 2);
+      REQUIRE(field_1d.numComponents == 1);
+      REQUIRE(field_1d.index == 2);
+
+      REQUIRE(field_2d.numComponents == 1);
+      REQUIRE(field_2d.index == 2);
+
+      REQUIRE(field_3d.numComponents == 1);
+      REQUIRE(field_3d.index == 2);
     }
     SECTION("AUXILIARY")
     {
-      Field<1> field(SCALAR, AUXILIARY, field_name);
+      Field<1> field_1d(SCALAR, AUXILIARY, field_name);
+      Field<2> field_2d(SCALAR, AUXILIARY, field_name);
+      Field<3> field_3d(SCALAR, AUXILIARY, field_name);
 
-      REQUIRE(field.numComponents == 1);
-      REQUIRE(field.index == 3);
+      REQUIRE(field_1d.numComponents == 1);
+      REQUIRE(field_1d.index == 3);
+
+      REQUIRE(field_2d.numComponents == 1);
+      REQUIRE(field_2d.index == 3);
+
+      REQUIRE(field_3d.numComponents == 1);
+      REQUIRE(field_3d.index == 3);
     }
   }
   SECTION("VECTOR")
   {
     SECTION("EXPLICIT_TIME_DEPENDENT")
     {
-      Field<1> field(SCALAR, EXPLICIT_TIME_DEPENDENT, field_name);
+      Field<1> field_1d(VECTOR, EXPLICIT_TIME_DEPENDENT, field_name);
+      Field<2> field_2d(VECTOR, EXPLICIT_TIME_DEPENDENT, field_name);
+      Field<3> field_3d(VECTOR, EXPLICIT_TIME_DEPENDENT, field_name);
 
-      REQUIRE(field.numComponents == 1);
-      REQUIRE(field.index == 4);
+      REQUIRE(field_1d.numComponents == 1);
+      REQUIRE(field_1d.index == 4);
+
+      REQUIRE(field_2d.numComponents == 2);
+      REQUIRE(field_2d.index == 4);
+
+      REQUIRE(field_3d.numComponents == 3);
+      REQUIRE(field_3d.index == 4);
     }
     SECTION("IMPLICIT_TIME_DEPENDENT")
     {
-      Field<1> field(SCALAR, IMPLICIT_TIME_DEPENDENT, field_name);
+      Field<1> field_1d(VECTOR, IMPLICIT_TIME_DEPENDENT, field_name);
+      Field<2> field_2d(VECTOR, IMPLICIT_TIME_DEPENDENT, field_name);
+      Field<3> field_3d(VECTOR, IMPLICIT_TIME_DEPENDENT, field_name);
 
-      REQUIRE(field.numComponents == 1);
-      REQUIRE(field.index == 5);
+      REQUIRE(field_1d.numComponents == 1);
+      REQUIRE(field_1d.index == 5);
+
+      REQUIRE(field_2d.numComponents == 2);
+      REQUIRE(field_2d.index == 5);
+
+      REQUIRE(field_3d.numComponents == 3);
+      REQUIRE(field_3d.index == 5);
     }
     SECTION("TIME_INDEPENDENT")
     {
-      Field<1> field(SCALAR, TIME_INDEPENDENT, field_name);
+      Field<1> field_1d(VECTOR, TIME_INDEPENDENT, field_name);
+      Field<2> field_2d(VECTOR, TIME_INDEPENDENT, field_name);
+      Field<3> field_3d(VECTOR, TIME_INDEPENDENT, field_name);
 
-      REQUIRE(field.numComponents == 1);
-      REQUIRE(field.index == 6);
+      REQUIRE(field_1d.numComponents == 1);
+      REQUIRE(field_1d.index == 6);
+
+      REQUIRE(field_2d.numComponents == 2);
+      REQUIRE(field_2d.index == 6);
+
+      REQUIRE(field_3d.numComponents == 3);
+      REQUIRE(field_3d.index == 6);
     }
     SECTION("AUXILIARY")
     {
-      Field<1> field(SCALAR, AUXILIARY, field_name);
+      Field<1> field_1d(VECTOR, AUXILIARY, field_name);
+      Field<2> field_2d(VECTOR, AUXILIARY, field_name);
+      Field<3> field_3d(VECTOR, AUXILIARY, field_name);
 
-      REQUIRE(field.numComponents == 1);
-      REQUIRE(field.index == 7);
+      REQUIRE(field_1d.numComponents == 1);
+      REQUIRE(field_1d.index == 7);
+
+      REQUIRE(field_2d.numComponents == 2);
+      REQUIRE(field_2d.index == 7);
+
+      REQUIRE(field_3d.numComponents == 3);
+      REQUIRE(field_3d.index == 7);
     }
+  }
+  SECTION("Invalid field type")
+  {
+    const auto invalid_field_type = static_cast<fieldType>(-1);
+
+    REQUIRE_THROWS_AS(Field<3>(invalid_field_type, EXPLICIT_TIME_DEPENDENT, field_name),
+                      std::invalid_argument);
+  }
+
+  SECTION("Invalid PDE type")
+  {
+    const auto invalid_PDE_type = static_cast<PDEType>(-1);
+
+    REQUIRE_THROWS_AS(Field<3>(SCALAR, invalid_PDE_type, field_name),
+                      std::invalid_argument);
   }
 }
