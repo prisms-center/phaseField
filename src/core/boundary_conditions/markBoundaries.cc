051c8857daf5882f2f566ff105ebbcda11ff4e45
Jason Landini <landinjm@umich.edu>
2024-11-26 10:15:36 -0500

    Rearranging core library

---
+++ src/core/boundary_conditions/markBoundaries.cc
@@ -0,0 +1,33 @@
+// methods to mark boundaries
+
+#include <core/initial_conditions/initialConditions.h>
+#include <core/matrixFreePDE.h>
+
+// methods to mark boundaries
+// methods to mark boundaries
+template <int dim, int degree>
+void
+MatrixFreePDE<dim, degree>::markBoundaries(
+  parallel::distributed::Triangulation<dim> &tria) const
+{
+  for (const auto &cell : tria.active_cell_iterators())
+    {
+      // Mark all of the faces
+      for (unsigned int face_number = 0; face_number < GeometryInfo<dim>::faces_per_cell;
+           ++face_number)
+        {
+          for (unsigned int i = 0; i < dim; i++)
+            {
+              if (std::fabs(cell->face(face_number)->center()(i) - (0)) < 1e-12)
+                {
+                  cell->face(face_number)->set_boundary_id(2 * i);
+                }
+              else if (std::fabs(cell->face(face_number)->center()(i) -
+                                 (userInputs.domain_size[i])) < 1e-12)
+                {
+                  cell->face(face_number)->set_boundary_id(2 * i + 1);
+                }
+            }
+        }
+    }
+}
---
+++ src/core/boundary_conditions/vectorBCFunction.cc
@@ -0,0 +1,45 @@
+/*
+ * vectorBCFunction.cc
+ *
+ *  Created on: Feb 22, 2017
+ *      Author: stephendewitt
+ */
+
+#include <deal.II/base/function.h>
+#include <deal.II/lac/vector.h>
+
+#include <core/boundary_conditions/vectorBCFunction.h>
+
+template <int dim>
+vectorBCFunction<dim>::vectorBCFunction(const std::vector<double> &BC_values)
+  : dealii::Function<dim>(dim)
+  , BC_values(BC_values)
+{}
+
+template <int dim>
+void
+vectorBCFunction<dim>::vector_value([[maybe_unused]] const dealii::Point<dim> &p,
+                                    dealii::Vector<double> &values) const
+{
+  for (unsigned int i = 0; i < dim; i++)
+    {
+      values(i) = BC_values[i];
+    }
+}
+
+template <int dim>
+void
+vectorBCFunction<dim>::vector_value_list(
+  const std::vector<dealii::Point<dim>> &points,
+  std::vector<dealii::Vector<double>>   &value_list) const
+{
+  const unsigned int n_points = points.size();
+  for (unsigned int p = 0; p < n_points; ++p)
+    {
+      vectorBCFunction<dim>::vector_value(points[p], value_list[p]);
+    }
+}
+
+template class vectorBCFunction<1>;
+template class vectorBCFunction<2>;
+template class vectorBCFunction<3>;
