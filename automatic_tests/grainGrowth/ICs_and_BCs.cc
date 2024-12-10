702f4145f66cfeff07a741ae0801176ba8f6ab4a
Jason Landini <landinjm@umich.edu>
2024-12-10 14:50:25 -0500

    adding more apps

---
+++ automatic_tests/grainGrowth/ICs_and_BCs.cc
@@ -0,0 +1,181 @@
+// ===========================================================================
+// FUNCTION FOR INITIAL CONDITIONS
+// ===========================================================================
+
+template <int dim, int degree>
+void
+customPDE<dim, degree>::setInitialCondition([[maybe_unused]] const Point<dim>  &p,
+                                            [[maybe_unused]] const unsigned int index,
+                                            [[maybe_unused]] double            &scalar_IC,
+                                            [[maybe_unused]] Vector<double>    &vector_IC)
+{
+  // ---------------------------------------------------------------------
+  // ENTER THE INITIAL CONDITIONS HERE
+  // ---------------------------------------------------------------------
+  // Enter the function describing conditions for the fields at point "p".
+  // Use "if" statements to set the initial condition for each variable
+  // according to its variable index
+
+  // The initial condition is a set of overlapping circles/spheres defined
+  // by a hyperbolic tangent function. The center of each circle/sphere is
+  // given by "center_list" and its radius is given by "radius".
+
+  if (index < 5)
+    {
+      std::vector<Point<dim>> center_list;
+
+      // The big grains
+      {
+        Point<dim> center(0.2, 0.15);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.25, 0.7);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.5, 0.5);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.6, 0.85);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.85, 0.35);
+        center_list.push_back(center);
+      }
+
+      // The medium grains
+      {
+        Point<dim> center(0.08, 0.92);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.75, 0.6);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.75, 0.1);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.2, 0.45);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.85, 0.85);
+        center_list.push_back(center);
+      }
+
+      // The small grains
+      {
+        Point<dim> center(0.55, 0.05);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.1, 0.35);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.95, 0.65);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.9, 0.15);
+        center_list.push_back(center);
+      }
+      {
+        Point<dim> center(0.45, 0.25);
+        center_list.push_back(center);
+      }
+
+      std::vector<double> rad = {0.14,
+                                 0.14,
+                                 0.14,
+                                 0.14,
+                                 0.14,
+                                 0.08,
+                                 0.08,
+                                 0.08,
+                                 0.08,
+                                 0.08,
+                                 0.05,
+                                 0.05,
+                                 0.05,
+                                 0.05,
+                                 0.05};
+
+      double dist = 0.0;
+      scalar_IC   = 0;
+
+      for (unsigned int dir = 0; dir < dim; dir++)
+        {
+          dist += (p[dir] - center_list[index][dir] * userInputs.domain_size[dir]) *
+                  (p[dir] - center_list[index][dir] * userInputs.domain_size[dir]);
+        }
+      dist = std::sqrt(dist);
+
+      scalar_IC +=
+        0.5 * (1.0 - std::tanh((dist - rad[index] * userInputs.domain_size[0]) / 0.5));
+
+      dist = 0.0;
+      for (unsigned int dir = 0; dir < dim; dir++)
+        {
+          dist += (p[dir] - center_list[index + 5][dir] * userInputs.domain_size[dir]) *
+                  (p[dir] - center_list[index + 5][dir] * userInputs.domain_size[dir]);
+        }
+      dist = std::sqrt(dist);
+
+      scalar_IC +=
+        0.5 *
+        (1.0 - std::tanh((dist - rad[index + 5] * userInputs.domain_size[0]) / 0.5));
+
+      dist = 0.0;
+      for (unsigned int dir = 0; dir < dim; dir++)
+        {
+          dist += (p[dir] - center_list[index + 10][dir] * userInputs.domain_size[dir]) *
+                  (p[dir] - center_list[index + 10][dir] * userInputs.domain_size[dir]);
+        }
+      dist = std::sqrt(dist);
+
+      scalar_IC +=
+        0.5 *
+        (1.0 - std::tanh((dist - rad[index + 10] * userInputs.domain_size[0]) / 0.5));
+    }
+  else
+    {
+      scalar_IC = 0.0;
+    }
+
+  // --------------------------------------------------------------------------
+}
+
+// ===========================================================================
+// FUNCTION FOR NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS
+// ===========================================================================
+
+template <int dim, int degree>
+void
+customPDE<dim, degree>::setNonUniformDirichletBCs(
+  [[maybe_unused]] const Point<dim>  &p,
+  [[maybe_unused]] const unsigned int index,
+  [[maybe_unused]] const unsigned int direction,
+  [[maybe_unused]] const double       time,
+  [[maybe_unused]] double            &scalar_BC,
+  [[maybe_unused]] Vector<double>    &vector_BC)
+{
+  // --------------------------------------------------------------------------
+  // ENTER THE NON-UNIFORM DIRICHLET BOUNDARY CONDITIONS HERE
+  // --------------------------------------------------------------------------
+  // Enter the function describing conditions for the fields at point "p".
+  // Use "if" statements to set the boundary condition for each variable
+  // according to its variable index. This function can be left blank if there
+  // are no non-uniform Dirichlet boundary conditions. For BCs that change in
+  // time, you can access the current time through the variable "time". The
+  // boundary index can be accessed via the variable "direction", which starts
+  // at zero and uses the same order as the BC specification in parameters.in
+  // (i.e. left = 0, right = 1, bottom = 2, top = 3, front = 4, back = 5).
+
+  // -------------------------------------------------------------------------
+}
---
+++ automatic_tests/precipitateEvolution_pfunction/PLibrary/PLibrary.hh
@@ -0,0 +1,40 @@
+// created: 2017-1-11 12:28:14
+// version: issue-1
+// url: https://github.com/prisms-center/IntegrationTools
+// commit: 34f71553d3b1b3cdbbc6a02cfb8be793a16396b5
+
+#ifndef PLIBRARY_HH
+#define PLIBRARY_HH
+
+#include <cstring>
+#include <field_input/IntegrationTools/PFunction.hh>
+#include <field_input/IntegrationTools/PPieceWise.hh>
+
+namespace PRISMS
+{
+
+  /// Library where you can find functions and basis sets
+  ///
+  namespace PLibrary
+  {
+    // Use these functions to checkout objects which manage their own memory
+
+    void
+    checkout(std::string name, PSimpleFunction<double *, double> &simplefunc);
+
+    void
+    checkout(std::string name, PFunction<double *, double> &func);
+
+    // Use these functions to checkout new 'Base' objects which the user must delete
+
+    void
+    checkout(std::string name, PSimpleBase<double *, double> *&simplefunc);
+
+    void
+    checkout(std::string name, PFuncBase<double *, double> *&func);
+
+  } // namespace PLibrary
+
+} // namespace PRISMS
+
+#endif
