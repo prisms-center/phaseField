424c97af25dd1391bdf5a4ed9cc906768474223f
landinjm <landinjm@umich.edu>
2024-12-17 07:26:50 -0500

    [WIP] Cleaning up vtk file read-in

--- include/field_input/IntegrationTools/datastruc/PNDArray.hh
+++ include/field_input/IntegrationTools/datastruc/PNDArray.hh
@@ -1,4 +1,3 @@
-
 #ifndef PNDArray_HH
 #define PNDArray_HH
 
@@ -6,171 +5,171 @@
 #include <iostream>
 #include <vector>
 
-namespace PRISMS
+/**
+ * \brief A Tensor class, takes int valued IndexContainer and returns OutType value.
+ */
+template <class OutType>
+class PNDArray
 {
+private:
+  std::vector<int>     _dim;    // dimension along each compenent
+  std::vector<int>     _unroll; // used to translate from tensor indices to linear index
+  std::vector<OutType> _val;    // unrolled list of coefficients (first index is outer
+                                // loop)
+  int _order;                   // _dim.size()
+  int _volume;                  // _coeff_tensor.size() = product_i(_dim[i])
 
-  /// A Tensor class,
-  ///    takes int valued IndexContainer and returns OutType value
-  ///
-  template <class OutType>
-  class PNDArray
+public:
+  PNDArray()
+    : _order(0)
+    , _volume(0)
+  {}
+
+  PNDArray(const std::vector<int> &dim)
   {
-    std::vector<int>     _dim;    // dimension along each compenent
-    std::vector<int>     _unroll; // used to translate from tensor indices to linear index
-    std::vector<OutType> _val;    // unrolled list of coefficients (first index is outer
-                                  // loop)
-    int _order;                   // _dim.size()
-    int _volume;                  // _coeff_tensor.size() = product_i(_dim[i])
+    resize(dim);
+  }
 
-  public:
-    PNDArray()
-      : _order(0)
-      , _volume(0)
-    {}
+  [[nodiscard]] PNDArray(const std::vector<int> &dim, const std::vector<OutType> &value)
+  {
+    resize(dim);
+    if (_volume != value.size())
+      {
+        std::cerr << "Error in PNDArray(const std::vector<int> &dim, const "
+                     "std::vector<OutType> &value)."
+                  << std::endl;
+        std::cerr << "  value.size() does not match volume based on dim." << std::endl;
+        exit(1);
+      }
+    _val = value;
+  }
 
-    PNDArray(const std::vector<int> &dim)
-    {
-      resize(dim);
-    }
+  [[nodiscard]] int
+  order() const
+  {
+    return _order;
+  }
 
-    [[nodiscard]] PNDArray(const std::vector<int> &dim, const std::vector<OutType> &value)
-    {
-      resize(dim);
-      if (_volume != value.size())
-        {
-          std::cerr << "Error in PNDArray(const std::vector<int> &dim, const "
-                       "std::vector<OutType> &value)."
-                    << std::endl;
-          std::cerr << "  value.size() does not match volume based on dim." << std::endl;
-          exit(1);
-        }
-      _val = value;
-    }
+  [[nodiscard]] int
+  volume() const
+  {
+    return _volume;
+  }
 
-    [[nodiscard]] int
-    order() const
-    {
-      return _order;
-    }
+  void
+  resize(const std::vector<int> &dim)
+  {
+    _dim    = dim;
+    _order  = _dim.size();
+    _volume = calc_volume(_dim);
+    _val.resize(_volume);
+    generate_unroll();
+  }
 
-    [[nodiscard]] int
-    volume() const
-    {
-      return _volume;
-    }
+  void
+  reshape(const std::vector<int> &dim)
+  {
+    if (calc_volume(dim) == _volume)
+      {
+        _dim   = dim;
+        _order = _dim.size();
+        generate_unroll();
+      }
+    else
+      {
+        std::cerr << "Error in PNDArray::reshape. Volume is not equivalent." << std::endl;
+        exit(1);
+      }
+  }
 
-    void
-    resize(const std::vector<int> &dim)
-    {
-      _dim    = dim;
-      _order  = _dim.size();
-      _volume = calc_volume(_dim);
-      _val.resize(_volume);
-      generate_unroll();
-    }
+  void
+  clear()
+  {
+    _val.clear();
+    _dim.clear();
+    _unroll.clear();
+    _order  = 0;
+    _volume = 0;
+  }
 
-    void
-    reshape(const std::vector<int> &dim)
-    {
-      if (calc_volume(dim) == _volume)
-        {
-          _dim   = dim;
-          _order = _dim.size();
-          generate_unroll();
-        }
-      else
-        {
-          std::cerr << "Error in PNDArray::reshape. Volume is not equivalent."
-                    << std::endl;
-          exit(1);
-        }
-    }
+  [[nodiscard]] const std::vector<int> &
+  dim() const
+  {
+    return _dim;
+  }
 
-    void
-    clear()
-    {
-      _val.clear();
-      _dim.clear();
-      _unroll.clear();
-      _order  = 0;
-      _volume = 0;
-    }
+  [[nodiscard]] int
+  dim(int i) const
+  {
+    return _dim[i];
+  }
 
-    [[nodiscard]] const std::vector<int> &
-    dim() const
-    {
-      return _dim;
-    }
+  OutType &
+  operator()(int i)
+  {
+    return _val[i];
+  }
 
-    [[nodiscard]] int
-    dim(int i) const
-    {
-      return _dim[i];
-    }
+  template <class IndexContainer>
+  OutType &
+  operator()(const IndexContainer &term)
+  {
+    return _val[linear_index(term)];
+  }
 
-    OutType &
-    operator()(int i)
-    {
-      return _val[i];
-    }
-
-    template <class IndexContainer>
-    OutType &
-    operator()(const IndexContainer &term)
-    {
-      return _val[linear_index(term)];
-    }
-
-    template <class IndexContainer>
-    int
-    linear_index(const IndexContainer &term) const
-    {
-      int lindex = 0;
-      for (unsigned int i = 0; i < _unroll.size(); i++)
+  template <class IndexContainer>
+  int
+  linear_index(const IndexContainer &term) const
+  {
+    int lindex = 0;
+    for (unsigned int i = 0; i < _unroll.size(); i++)
+      {
         lindex += term[i] * _unroll[i];
-      return lindex;
-    }
+      }
+    return lindex;
+  }
 
-    template <class IndexContainer>
-    void
-    tensor_indices(int lindex, IndexContainer &term) const
-    { // assumes term.size() == order()  (the tensor order)
-      // not sure if this is how we want to do it, but it avoids assuming push_back() or
-      // resize()
+  template <class IndexContainer>
+  void
+  tensor_indices(int lindex, IndexContainer &term) const
+  { // assumes term.size() == order()  (the tensor order)
+    // not sure if this is how we want to do it, but it avoids assuming push_back() or
+    // resize()
 
-      for (int i = 0; i < _unroll.size(); i++)
-        {
-          term[i] = lindex / _unroll[i];
-          lindex -= term[i] * _unroll[i];
-        }
-    }
+    for (int i = 0; i < _unroll.size(); i++)
+      {
+        term[i] = lindex / _unroll[i];
+        lindex -= term[i] * _unroll[i];
+      }
+  }
 
-  private:
-    int
-    calc_volume(const std::vector<int> &dim)
-    {
-      if (dim.size() == 0)
-        {
-          return 0;
-        }
+private:
+  int
+  calc_volume(const std::vector<int> &dim)
+  {
+    if (dim.empty())
+      {
+        return 0;
+      }
 
-      int vol = 1;
-      for (unsigned int i = 0; i < dim.size(); i++)
-        {
-          vol *= dim[i];
-        }
-      return vol;
-    }
+    int vol = 1;
+    for (int i : dim)
+      {
+        vol *= i;
+      }
+    return vol;
+  }
 
-    void
-    generate_unroll()
-    {
-      _unroll.resize(_dim.size());
-      _unroll[_dim.size() - 1] = 1;
-      for (int i = _dim.size() - 2; i >= 0; i--)
+  void
+  generate_unroll()
+  {
+    _unroll.resize(_dim.size());
+    _unroll[_dim.size() - 1] = 1;
+    for (int i = _dim.size() - 2; i >= 0; i--)
+      {
         _unroll[i] = _unroll[i + 1] * _dim[i + 1];
-    }
-  };
-} // namespace PRISMS
+      }
+  }
+};
 
 #endif
