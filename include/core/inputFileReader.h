389726bbd85f7560e77361d22b08b3343c82682a
Xander <102053371+fractalsbyx@users.noreply.github.com>
2025-01-01 17:04:44 -0500

    Reorganize inputFileReader and userInputParameters (#375)
    * replace vector of model constants with map
    
    note: use AssertThrow rather than Assert for release exceptions
    
    * minor refactor of inputFileReader and userInputs
    
    Mostly reorganization. Split the parameter_handler declarations into several functions rather than one 500-line function.
    Object-oriented some functionality. Removed get_number_of_entries.
    Renamed a few functions.

--- include/core/inputFileReader.h
+++ include/core/inputFileReader.h
@@ -6,11 +6,13 @@
 #include <deal.II/base/parameter_handler.h>
 
 #include <core/variableAttributeLoader.h>
+#include <set>
 #include <string>
 #include <vector>
 
 /**
- * \brief Input file reader.
+ * \brief Parameters file reader. Declares parameter names in a dealii parameter_handler
+ * and parses the file for the values. Variable assignment occurs in userInputParameters.
  */
 class inputFileReader
 {
@@ -26,35 +28,23 @@ public:
    * \brief Method to get a list of entry values from multiple subsections in an input
    * file.
    */
-  [[nodiscard]] static std::vector<std::string>
-  get_subsection_entry_list(const std::string &parameters_file_name,
-                            const std::string &subsec_name,
+  [[nodiscard]] std::vector<std::string>
+  get_subsection_entry_list(const std::string &subsec_name,
                             const std::string &entry_name,
                             const std::string &default_entry);
-
-  /**
-   * \brief Method to count the number of related entries in an input file.
-   */
-  [[nodiscard]] static unsigned int
-  get_number_of_entries(const std::string &parameters_file_name,
-                        const std::string &keyword,
-                        const std::string &entry_name);
 
   /**
    * \brief Get the trailing part of the entry name after a specified string (used to
    * extract the model constant names).
    */
-  [[nodiscard]] static std::vector<std::string>
-  get_entry_name_ending_list(const std::string &parameters_file_name,
-                             const std::string &keyword,
-                             const std::string &entry_name_begining);
+  [[nodiscard]] std::set<std::string>
+  get_model_constant_names();
 
   /**
    * \brief Method to declare the parameters to be read from an input file.
    */
   void
-  declare_parameters(dealii::ParameterHandler &parameter_handler,
-                     const unsigned int        num_of_constants) const;
+  declare_parameters();
 
   /**
    * \brief Method to check if a line has the desired contents and if so, extract it.
@@ -78,11 +68,83 @@ public:
   static bool
   check_keyword_match(std::string &line, const std::string &keyword);
 
+  /**
+   * \brief Declare parameters for the mesh.
+   */
+  void
+  declare_mesh();
+
+  /**
+   * \brief Declare parameters for timestepping.
+   */
+  void
+  declare_time_discretization();
+
+  /**
+   * \brief Declare parameters for linear and nonlinear solvers.
+   */
+  void
+  declare_solver_parameters();
+
+  /**
+   * \brief Declare parameters for outputs.
+   */
+  void
+  declare_output_parameters();
+
+  /**
+   * \brief Declare parameters for loading ICs from files.
+   */
+  void
+  declare_load_IC_parameters();
+
+  /**
+   * \brief Declare parameters for checkpoints.
+   */
+  void
+  declare_checkpoint_parameters();
+
+  /**
+   * \brief Declare parameters for boundary conditions.
+   */
+  void
+  declare_BC_parameters();
+
+  /**
+   * \brief Declare parameters for pinned points.
+   */
+  void
+  declare_pinning_parameters();
+
+  /**
+   * \brief Declare parameters for nucleation
+   */
+  void
+  declare_nucleation_parameters();
+
+  /**
+   * \brief Declare parameters for grain remapping.
+   */
+  void
+  declare_grain_remapping_parameters();
+
+  /**
+   * \brief Declare parameters for grain structure loading.
+   */
+  void
+  declare_grain_loading_parameters();
+
+  /**
+   * \brief Declare parameters for user-defined model constants.
+   */
+  void
+  declare_model_constants();
+
+  const std::string        parameters_file_name;
   const AttributesList    &var_attributes;
   const AttributesList    &pp_attributes;
   dealii::ParameterHandler parameter_handler;
-  unsigned int             num_constants;
-  std::vector<std::string> model_constant_names;
+  std::set<std::string>    model_constant_names;
   unsigned int             number_of_dimensions;
 };
 
