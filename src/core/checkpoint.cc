fad394cde94725960d19339a3723b04699c09cee
Jason Landini <123970616+landinjm@users.noreply.github.com>
2025-01-15 16:44:34 -0500

    Fix cmake for intel & clang compilers (#403)
    * Updating cmakelists. In the future we should make more use of macros
    
    * fixing template instantations
    
    * fix gitignore

--- src/core/checkpoint.cc
+++ src/core/checkpoint.cc
@@ -273,3 +273,21 @@ MatrixFreePDE<dim, degree>::verify_checkpoint_file_exists(const std::string &fil
                              filename + "> does not appear to exist!"));
     }
 }
+
+template class MatrixFreePDE<2, 1>;
+template class MatrixFreePDE<3, 1>;
+
+template class MatrixFreePDE<2, 2>;
+template class MatrixFreePDE<3, 2>;
+
+template class MatrixFreePDE<3, 3>;
+template class MatrixFreePDE<2, 3>;
+
+template class MatrixFreePDE<3, 4>;
+template class MatrixFreePDE<2, 4>;
+
+template class MatrixFreePDE<3, 5>;
+template class MatrixFreePDE<2, 5>;
+
+template class MatrixFreePDE<3, 6>;
+template class MatrixFreePDE<2, 6>;
--- src/core/postprocessing/computeIntegral.cc
+++ src/core/postprocessing/computeIntegral.cc
@@ -45,3 +45,21 @@ MatrixFreePDE<dim, degree>::computeIntegral(
 
   integratedField = value;
 }
+
+template class MatrixFreePDE<2, 1>;
+template class MatrixFreePDE<3, 1>;
+
+template class MatrixFreePDE<2, 2>;
+template class MatrixFreePDE<3, 2>;
+
+template class MatrixFreePDE<3, 3>;
+template class MatrixFreePDE<2, 3>;
+
+template class MatrixFreePDE<3, 4>;
+template class MatrixFreePDE<2, 4>;
+
+template class MatrixFreePDE<3, 5>;
+template class MatrixFreePDE<2, 5>;
+
+template class MatrixFreePDE<3, 6>;
+template class MatrixFreePDE<2, 6>;
