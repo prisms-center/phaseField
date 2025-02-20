0410f460d055a243e3ffb3dfc665136a2609e1cd
landinjm <landinjm@umich.edu>
2025-02-20 17:04:42 -0500

    updating script

--- contrib/utilities/update_copyright.sh
+++ contrib/utilities/update_copyright.sh
@@ -14,27 +14,37 @@ if test ! -d src -o ! -d include -o ! -d applications ; then
   exit 0
 fi
 
+if ! [ -x "$(command -v clang-format)" ] ; then
+    echo "make sure clang-format is in the path"
+    exit 1
+fi
+
 # Directories containing the .cc and .h files
 TARGET_DIRS=("src" "include" "tests" "applications")
 
-for TARGET_DIR in "${TARGET_DIRS[@]}"; do
+for TARGET_DIR in "${TARGET_DIRS[@]}" ; do
   # Check if the directory exists
-  if [ ! -d "$TARGET_DIR" ]; then
-    echo "Directory $TARGET_DIR does not exist."
-    exit 1
+  if [ ! -d "$TARGET_DIR" ] ; then
+    echo "Directory $TARGET_DIR does not exist"
+    exit 2
   fi
 
   # Find all .cc and .h files and insert the text
-  find "$TARGET_DIR" -type f \( -name "*.cc" -o -name "*.h" \) | while read -r file; do
+  find "$TARGET_DIR" -type f \( -name "*.cc" -o -name "*.h" \) | while read -r file ; do
     last_year=`git log -n 3 --date=short --format="format:%cd %s" ${file} | \
       egrep -i -v "update.*copyright|copyright.*update|Update license headers" | \
       head -n 1 | \
       perl -p -e 's/^(\d\d\d\d)-.*/\1/g;'`
     INSERT_TEXT="// SPDX-FileCopyrightText: © ${last_year} PRISMS Center at the University of Michigan
-// SPDX-License-Identifier: GNU Lesser General Public Version 2.1\n\n"
+// SPDX-License-Identifier: GNU Lesser General Public Version 2.1
+"
     # Insert the text at the beginning of the file
-    echo -e "$INSERT_TEXT$(cat "$file")" > "$file"
+    printf "%s\n%s" "$INSERT_TEXT" "$(cat "$file")" > "$file"
     echo "Inserted text into $file"
+
+    # Run clang-format on the file
+    clang-format -i "$file"
+    echo "Formatted $file with clang-format"
   done
 done
 
