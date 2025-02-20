b410559a57095404752ef325da6f208e264b1584
landinjm <landinjm@umich.edu>
2025-02-20 17:11:04 -0500

    updates

--- applications/nucleationModel_preferential/nucleation.cc
+++ applications/nucleationModel_preferential/nucleation.cc
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: © 2024 PRISMS Center at the University of Michigan
+// SPDX-FileCopyrightText: © 2025 PRISMS Center at the University of Michigan
 // SPDX-License-Identifier: GNU Lesser General Public Version 2.1
 
 // =================================================================================
