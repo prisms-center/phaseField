4d8aff909650a897b856d90a25dd0e6dc44cb75c
landinjm <landinjm@umich.edu>
2025-03-28 18:20:45 -0400

    updating cppcheck since it wasn't working properly and clang-format

--- contrib/utilities/cppcheck.sh
+++ contrib/utilities/cppcheck.sh
@@ -6,7 +6,9 @@
 #
 #
 # Usage:
-# ./contrib/utilities/cppcheck.sh
+# ./contrib/utilities/cppcheck.sh CMAKE_ARGS
+#   with:
+#     CMAKE_ARGS are the optional arguments passed to cmake
 #
 
 if test ! -d src -o ! -d include -o ! -d applications; then
@@ -19,12 +21,30 @@ if ! [ -x "$(command -v cppcheck)" ]; then
   exit 1
 fi
 
+# Construct the cmake arguments
+ARGS=("-D" "CMAKE_EXPORT_COMPILE_COMMANDS=ON" "-D" "CMAKE_BUILD_TYPE=Debug" "$@")
+
+# Compile
+if [ -f CMakeCache.txt ]; then
+  rm -f CMakeCache.txt
+fi
+cmake "${ARGS[@]}" . || (
+  echo "cmake failed!"
+  false
+) || exit 2
+cmake --build . || exit 3
+
+# Create a file that contains all the headers to ensure that cppcheck runs on all headers
+(
+  cd include
+  find . -name '*.h'
+) | grep -v allheaders.h | sed 's|^./|#include <|' | sed 's|$|>|' >include/allheaders.h
+
 # Run cppcheck
-# TODO: Check the applications
 cppcheck \
   --enable=all --language=c++ --std=c++17 \
   --suppress=missingIncludeSystem --suppress=unknownMacro \
-  include src tests/automatic_tests tests/performance_tests >"output.txt" 2>&1
+  --project=compile_commands.json >"output.txt" 2>&1
 
 # grep interesting errors and make sure we remove duplicates:
 grep -E '(warning|error|style|performance|portability): ' output.txt | sort | uniq >cppcheck.log
@@ -32,7 +52,7 @@ grep -E '(warning|error|style|performance|portability): ' output.txt | sort | un
 # If we have errors, report them and set exit status to failure
 if [ -s cppcheck.log ]; then
   cat cppcheck.log
-  exit 2
+  exit 4
 fi
 
 echo "OK"
--- include/prismspf/core/nonuniform_dirichlet.h
+++ include/prismspf/core/nonuniform_dirichlet.h
@@ -8,6 +8,7 @@
 #include <deal.II/lac/vector.h>
 
 #include <prismspf/core/type_enums.h>
+
 #include <prismspf/user_inputs/user_input_parameters.h>
 
 #include <prismspf/config.h>
