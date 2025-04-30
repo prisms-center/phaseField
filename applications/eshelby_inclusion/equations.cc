bae175d984cf7d64190541ccb9f1b6999eab0295
Jason Landini <123970616+landinjm@users.noreply.github.com>
2025-04-30 12:06:21 -0400

    Fix a few applications and adding CI (#499)
    * updating scripts
    
    * mostly fixing allen cahn app
    
    * a few fixes to python script
    
    * failing in a cool way
    
    * adding workflow and a few example applications
    
    * eshelby app
    
    * added proper exit code
    
    * updating application list
    
    * missed an import

---
+++ applications/eshelby_inclusion/equations.cc
@@ -0,0 +1,103 @@
+// SPDX-FileCopyrightText: © 2025 PRISMS Center at the University of Michigan
+// SPDX-License-Identifier: GNU Lesser General Public Version 2.1
+
+#include "custom_pde.h"
+
+#include <prismspf/core/type_enums.h>
+#include <prismspf/core/variable_attribute_loader.h>
+
+#include <prismspf/config.h>
+
+PRISMS_PF_BEGIN_NAMESPACE
+
+void
+customAttributeLoader::loadVariableAttributes()
+{
+  set_variable_name(0, "u");
+  set_variable_type(0, VECTOR);
+  set_variable_equation_type(0, TIME_INDEPENDENT);
+  set_dependencies_gradient_term_RHS(0, "grad(u)");
+  set_dependencies_gradient_term_LHS(0, "grad(change(u))");
+}
+
+template <unsigned int dim, unsigned int degree, typename number>
+void
+customPDE<dim, degree, number>::compute_explicit_RHS(
+  [[maybe_unused]] variableContainer<dim, degree, number> &variable_list,
+  [[maybe_unused]] const dealii::Point<dim, dealii::VectorizedArray<number>> &q_point_loc)
+  const
+{}
+
+template <unsigned int dim, unsigned int degree, typename number>
+void
+customPDE<dim, degree, number>::compute_nonexplicit_RHS(
+  [[maybe_unused]] variableContainer<dim, degree, number> &variable_list,
+  [[maybe_unused]] const dealii::Point<dim, dealii::VectorizedArray<number>> &q_point_loc,
+  [[maybe_unused]] types::index current_index) const
+{
+  if (current_index == 0)
+    {
+      vectorGrad ux = variable_list.get_vector_symmetric_gradient(0);
+
+      scalarValue dist_from_inclusion = constV<number>(0.0);
+      scalarValue inclusion_radius    = 10.0;
+      for (unsigned int i = 0; i < dim; i++)
+        {
+          dist_from_inclusion += (q_point_loc[i] - constV<number>(0.0)) *
+                                 (q_point_loc[i] - constV<number>(0.0));
+        }
+      dist_from_inclusion = std::sqrt(dist_from_inclusion);
+
+      vectorGrad transformation_strain;
+      for (unsigned int i = 0; i < dim; i++)
+        {
+          for (unsigned int j = 0; j < dim; j++)
+            {
+              if (i == j)
+                {
+                  transformation_strain[i][j] =
+                    0.01 * (0.5 + 0.5 *
+                                    (1.0 - std::exp(-20.0 * (dist_from_inclusion -
+                                                             inclusion_radius))) /
+                                    (1.0 + std::exp(-20.0 * (dist_from_inclusion -
+                                                             inclusion_radius))));
+                }
+              else
+                {
+                  transformation_strain[i][j] = 0.0;
+                }
+            }
+        }
+      vectorGrad stress;
+      compute_stress<dim, scalarValue>(CIJ, ux - transformation_strain, stress);
+      variable_list.set_vector_gradient_term(0, -stress);
+    }
+}
+
+template <unsigned int dim, unsigned int degree, typename number>
+void
+customPDE<dim, degree, number>::compute_nonexplicit_LHS(
+  [[maybe_unused]] variableContainer<dim, degree, number> &variable_list,
+  [[maybe_unused]] const dealii::Point<dim, dealii::VectorizedArray<number>> &q_point_loc,
+  [[maybe_unused]] types::index current_index) const
+{
+  if (current_index == 0)
+    {
+      vectorGrad change_ux = variable_list.get_vector_symmetric_gradient(0, CHANGE);
+      vectorGrad stress;
+      compute_stress<dim, scalarValue>(CIJ, change_ux, stress);
+      variable_list.set_vector_gradient_term(0, stress, CHANGE);
+    }
+}
+
+template <unsigned int dim, unsigned int degree, typename number>
+void
+customPDE<dim, degree, number>::compute_postprocess_explicit_RHS(
+  [[maybe_unused]] variableContainer<dim, degree, number> &variable_list,
+  [[maybe_unused]] const dealii::Point<dim, dealii::VectorizedArray<number>> &q_point_loc)
+  const
+{}
+
+INSTANTIATE_TRI_TEMPLATE(customPDE)
+
+PRISMS_PF_END_NAMESPACE
