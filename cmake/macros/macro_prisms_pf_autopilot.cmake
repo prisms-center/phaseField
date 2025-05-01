18b6ef8483541bddffdbfd5d1786c6f860d66ead
Jason Landini <123970616+landinjm@users.noreply.github.com>
2025-04-30 23:07:51 -0400

    CMake update for dependency autodetection (#514)
    * accidently deleted some unit tests
    
    * typo & autodetection of packages
    
    * applications will not depend on the core library for their dependencies
    
    * autopilot script

--- cmake/macros/macro_prisms_pf_autopilot.cmake
+++ cmake/macros/macro_prisms_pf_autopilot.cmake
@@ -2,6 +2,50 @@
 # Autopilot function that will set up an application
 #
 
-macro(prisms_pf_autopilot)
+macro(prisms_pf_autopilot PRISMS_PF_CORE_DIR)
+  # Enable compile commands export
   set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-endmacro()
+
+  include_directories(${CMAKE_BINARY_DIR})
+
+  # Set location of files
+  include_directories(${PRISMS_PF_CORE_DIR}/include)
+  include_directories(${PRISMS_PF_CORE_DIR}/src)
+  include_directories(${CMAKE_CURRENT_SOURCE_DIR})
+
+  # Find Caliper if enabled
+  if(${PRISMS_PF_WITH_CALIPER})
+    find_package(caliper REQUIRED)
+    include_directories(${CALIPER_INCLUDE_DIR})
+  endif()
+
+  # Set the location of the main.cc file
+  set(TARGET_SRC 
+    "${PRISMS_PF_CORE_DIR}/src/main.cc" 
+    "${CMAKE_CURRENT_SOURCE_DIR}/equations.cc" 
+    "${CMAKE_CURRENT_SOURCE_DIR}/ICs_and_BCs.cc"
+  )
+
+  # Set targets & link libraries for the build type
+  if(${PRISMS_PF_BUILD_DEBUG} STREQUAL "ON")
+    add_executable(main_debug ${TARGET_SRC})
+    set_property(TARGET main_debug PROPERTY OUTPUT_NAME main-debug)
+    deal_ii_setup_target(main_debug DEBUG)
+    target_link_libraries(main_debug ${PRISMS_PF_CORE_DIR}/libprisms-pf-debug.a)
+
+    if(${PRISMS_PF_WITH_CALIPER})
+      target_link_libraries(main_debug caliper)
+    endif()
+  endif()
+
+  if(${PRISMS_PF_BUILD_RELEASE} STREQUAL "ON")
+    add_executable(main_release ${TARGET_SRC})
+    set_property(TARGET main_release PROPERTY OUTPUT_NAME main)
+    deal_ii_setup_target(main_release RELEASE)
+    target_link_libraries(main_release ${PRISMS_PF_CORE_DIR}/libprisms-pf-release.a)
+
+    if(${PRISMS_PF_WITH_CALIPER})
+      target_link_libraries(main_release caliper)
+    endif()
+  endif()
+endmacro() 
