d3b2cdc476508b368d8e320b1c19c0bc2f902038
landinjm <landinjm@umich.edu>
2025-06-05 17:29:58 -0400

    working for vtk end

--- include/prismspf/field_input/read_vtk.h
+++ include/prismspf/field_input/read_vtk.h
@@ -2,11 +2,18 @@
 // SPDX-License-Identifier: GNU Lesser General Public Version 2.1
 
 #include <deal.II/base/exceptions.h>
+#include <deal.II/base/point.h>
+
+#include <prismspf/utilities/utilities.h>
 
 #include <filesystem>
+#include <vtkDataArray.h>
 #include <vtkNew.h>
+#include <vtkPointData.h>
 #include <vtkUnstructuredGrid.h>
 #include <vtkUnstructuredGridReader.h>
+
+PRISMS_PF_BEGIN_NAMESPACE
 
 template <unsigned int dim>
 class ReadUnstructuredVTK
@@ -45,6 +52,24 @@ public:
    */
   void
   print_vtk_file();
+
+  /**
+   * \brief Get the names of the scalars in the vtk file.
+   */
+  std::vector<std::string>
+  get_scalars_names();
+
+  /**
+   * \brief Get the names of the vectors in the vtk file.
+   */
+  std::vector<std::string>
+  get_vectors_names();
+
+  /**
+   * \brief Get scalar value for a given point
+   */
+  double
+  get_scalar_value(const dealii::Point<dim> &point, const std::string &scalar_name);
 
 private:
   /**
@@ -169,3 +194,66 @@ ReadUnstructuredVTK<dim>::print_vtk_file()
   // TODO (landinjm): Should we print only on rank 0?.
   reader->GetOutput()->PrintSelf(std::cout, vtkIndent());
 }
+
+template <unsigned int dim>
+inline std::vector<std::string>
+ReadUnstructuredVTK<dim>::get_scalars_names()
+{
+  std::vector<std::string> scalars_names(n_scalars);
+  for (unsigned int i = 0; i < n_scalars; ++i)
+    {
+      scalars_names[i] = reader->GetScalarsNameInFile(static_cast<int>(i));
+    }
+  return scalars_names;
+}
+
+template <unsigned int dim>
+inline std::vector<std::string>
+ReadUnstructuredVTK<dim>::get_vectors_names()
+{
+  std::vector<std::string> vectors_names(n_vectors);
+  for (unsigned int i = 0; i < n_vectors; ++i)
+    {
+      vectors_names[i] = reader->GetVectorsNameInFile(static_cast<int>(i));
+    }
+  return vectors_names;
+}
+
+template <unsigned int dim>
+inline double
+ReadUnstructuredVTK<dim>::get_scalar_value(const dealii::Point<dim> &point,
+                                           const std::string        &scalar_name)
+{
+  // Check that the scalar name is in the vtk file
+  auto scalars_names = get_scalars_names();
+  AssertThrow(std::find(scalars_names.begin(), scalars_names.end(), scalar_name) !=
+                scalars_names.end(),
+              dealii::ExcMessage(
+                "The provided vtk dataset does not contain a field named " +
+                scalar_name));
+
+  // Convet the dealii point to an array
+  std::array<double, 3> point_c_array = dealii_point_to_c_array<dim>(point);
+
+  // Set the active scalar and update the reader
+  reader->SetScalarsName(scalar_name.c_str());
+  reader->Update();
+
+  // Grab the output and point data
+  vtkUnstructuredGrid *output     = reader->GetOutput();
+  vtkPointData        *point_data = output->GetPointData();
+
+  // Find the point id in the vtk file
+  vtkIdType point_id = output->FindPoint(point_c_array.data());
+  AssertThrow(point_id >= 0, dealii::ExcMessage("No matching point found in VTK grid"));
+
+  // Get the data array
+  vtkDataArray *data_array = point_data->GetArray(scalar_name.c_str());
+  AssertThrow(data_array != nullptr,
+              dealii::ExcMessage(std::string("Data array not found: ") + scalar_name));
+
+  // Get the value of the scalar at the point
+  return data_array->GetComponent(point_id, 0);
+}
+
+PRISMS_PF_END_NAMESPACE
