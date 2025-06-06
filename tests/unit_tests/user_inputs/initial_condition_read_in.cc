ef6f7783f78eaeb4c5799d05e35498ea831831e5
landinjm <landinjm@umich.edu>
2025-06-05 22:34:20 -0400

    beginnng of unit test

---
+++ tests/unit_tests/user_inputs/initial_condition_read_in.cc
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: © 2025 PRISMS Center at the University of Michigan
+// SPDX-License-Identifier: GNU Lesser General Public Version 2.1
+
+#include <prismspf/config.h>
+
+#include "catch.hpp"
+
+PRISMS_PF_BEGIN_NAMESPACE
+
+PRISMS_PF_END_NAMESPACE
