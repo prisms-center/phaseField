17e6d89648961a6352d1f5c9d1840dbe4af270a9
landinjm <landinjm@umich.edu>
2025-07-30 18:51:20 -0400

    temp

---
+++ cmake/setup_cached_variables.cmake
@@ -0,0 +1,74 @@
+#
+# Setup cached variables. These are just the configuration
+# options for PRISMS-PF among some other things
+#
+
+# Basic configuration options
+option(PRISMS_PF_AUTODETECTION "Autodetection of PRISMS-PF dependencies." ON)
+message(STATUS "PRISMS_PF_AUTODETECTION = ${PRISMS_PF_AUTODETECTION}")
+
+option(
+    ADDITIONAL_OPTIMIZATIONS
+    "Whether the user wants to enable additional optimizations, or not."
+    OFF
+)
+message(STATUS "ADDITIONAL_OPTIMIZATIONS = ${ADDITIONAL_OPTIMIZATIONS}")
+
+option(
+    ADDITIONAL_DEGREES
+    "Wether the user wants to enable the compilation of additional element degrees or not."
+    OFF
+)
+message(STATUS "ADDITIONAL_DEGREES = ${ADDITIONAL_DEGREES}")
+
+option(
+    UNWRAP_COMPILER
+    "Whether the user wants to unwrap the compiler in compile_commands.json, or not."
+    OFF
+)
+message(STATUS "UNWRAP_COMPILER = ${UNWRAP_COMPILER}")
+
+# Setup the build type (debug, release, debugrelease)
+if("${CMAKE_BUILD_TYPE}" STREQUAL "")
+    set(CMAKE_BUILD_TYPE
+        "DebugRelease"
+        CACHE STRING
+        "Choose the type of build, options are: Debug, Release and DebugRelease."
+        FORCE
+    )
+endif()
+
+# Convert build type into the debug and release builds, which may or may
+# not be built.
+if(
+    "${CMAKE_BUILD_TYPE}" STREQUAL "Release"
+    OR "${CMAKE_BUILD_TYPE}" STREQUAL "Debug"
+    OR "${CMAKE_BUILD_TYPE}" STREQUAL "DebugRelease"
+)
+    message(STATUS "Setting up PRISMS-PF for ${CMAKE_BUILD_TYPE} mode.")
+else()
+    message(
+        FATAL_ERROR
+        "CMAKE_BUILD_TYPE must either be 'Release', 'Debug', or 'DebugRelease', but is set to '${CMAKE_BUILD_TYPE}'."
+    )
+endif()
+
+if(
+    "${CMAKE_BUILD_TYPE}" STREQUAL "Debug"
+    OR "${CMAKE_BUILD_TYPE}" STREQUAL "DebugRelease"
+)
+    set(PRISMS_PF_BUILD_DEBUG "ON")
+    list(APPEND PRISMS_PF_BUILD_TYPES "Debug")
+else()
+    set(PRISMS_PF_BUILD_DEBUG "OFF")
+endif()
+
+if(
+    "${CMAKE_BUILD_TYPE}" STREQUAL "Release"
+    OR "${CMAKE_BUILD_TYPE}" STREQUAL "DebugRelease"
+)
+    set(PRISMS_PF_BUILD_RELEASE "ON")
+    list(APPEND PRISMS_PF_BUILD_TYPES "Release")
+else()
+    set(PRISMS_PF_BUILD_RELEASE "OFF")
+endif()
---
+++ cmake/setup_prisms_pf.cmake
@@ -0,0 +1,21 @@
+#
+# Setup PRISMS-PF definitions
+#
+
+# General info
+set_if_empty(PRISMS_PF_PACKAGE_NAME "PRISMS-PF")
+
+# Grab the version
+file(STRINGS "${CMAKE_SOURCE_DIR}/VERSION" _version LIMIT_COUNT 1)
+set_if_empty(PRISMS_PF_VERSION "${_version}")
+
+# Decompose the version
+string(REGEX REPLACE "^([0-9]+)\\..*" "\\1"
+PRISMS_PF_VERSION_MAJOR "${PRISMS_PF_VERSION}"
+  )
+string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1"
+PRISMS_PF_VERSION_MINOR "${PRISMS_PF_VERSION}"
+  )
+string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1"
+PRISMS_PF_VERSION_SUBMINOR "${PRISMS_PF_VERSION}"
+  )
