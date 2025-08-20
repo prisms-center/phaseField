bde13d8a31dfe57ab811cc3a44b9f0951bf6b71a
landinjm <landinjm@umich.edu>
2025-08-20 16:34:51 -0400

    adding the main page

---
+++ doc/doxygen/main/main.h
@@ -0,0 +1,47 @@
+/** \mainpage PRISMS-PF
+
+\subsection welcome Welcome
+Welcome to the PRISMS-PF documentation! Click on any of the links in the sidebar and
+header to navigate the site. New users can start with the \ref installation and
+\ref tutorial sections.
+
+\subsection overview Overview
+PRISMS-PF is a powerful, massively parallel finite element code for conducting phase field
+and other related simulations of microstructural evolution. The phase field method is
+commonly used for predicting the evolution of microstructures under a wide range of
+environments and materials.
+
+The main goals of PRISMS-PF are to:
+- provide a **simple interface** for solving a **wide variety** of partial differential
+equations (PDEs)
+- have **parallel performance** that meets or exceeds that of other phase field codes
+- be **extensible** to allow users to add their own PDEs and modules
+- provide a **rich set of pre-built applications** for common phase field problems
+- **benchmark** code against other codes to ensure correctness
+- be **open source** and **freely available** to the community
+
+\remark While PRISMS-PF was conceived as a phase field code, it is by no means limited to
+phase field applications. Like deal.II positions itself as a general-purpose finite
+element library, PRISMS-PF is a general-purpose partial differential equation (PDE)
+solver.\n\n However, the numerics are primarily designed for phase field, and might be
+unsuitable for your application. If you find yourself in this position, please reach
+out over GitHub discussions or email!
+
+This code is developed by the PRedictive Integrated Structural Materials Science (PRISMS)
+Center at University of Michigan which is supported by the U.S. Department of Energy
+(DOE), Office of Basic Energy Sciences, Division of Materials Sciences and Engineering
+under Award #DE-SC0008637.
+
+\subsection links_contact Helpful Links & Contact Info
+[PRISMS Center Homepage](http://www.prisms-center.org/#/home)\n
+[GitHub Repository](https://github.com/prisms-center/phaseField)\n
+[GitHub Discussions](https://github.com/prisms-center/phaseField/discussions)\n
+[User Forum](https://groups.google.com/forum/#!forum/prisms-pf-users)\n
+[Community of Practice](https://materialscommons.org/public/communities/3)\n
+[PRISMS YouTube](https://www.youtube.com/@PRISMSCenter)\n\n
+External phase field links:\n
+[PFHub](https://pages.nist.gov/pfhub/)\n
+[PF Best Practices](https://pages.nist.gov/pf-recommended-practices/intro.html)\n\n
+Contact us at prisms-pf@umich.edu
+
+ */
