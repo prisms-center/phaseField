6b5aa56a29a787c527d4e2a267d67b3f99a968a3
Xander <102053371+fractalsbyx@users.noreply.github.com>
2025-09-12 14:59:05 -0400

    consolidated solve blocks (#700)

--- include/prismspf/core/solver_handler.h
+++ include/prismspf/core/solver_handler.h
@@ -23,6 +23,109 @@ PRISMS_PF_BEGIN_NAMESPACE
 template <unsigned int dim, unsigned int degree, typename number>
 class SolverContext;
 
+template <unsigned int dim, unsigned int degree, typename number>
+class SolveBlock
+{
+public:
+  /**
+   * @brief Constructor.
+   */
+  explicit SolveBlock(const SolverContext<dim, degree, number> &_solver_context,
+                      Types::Index                              block_index);
+
+  /**
+   * @brief Destructor.
+   */
+  ~SolveBlock() = default;
+
+  /**
+   * @brief Copy constructor.
+   *
+   * Deleted so solver instances aren't copied.
+   */
+  SolveBlock(const SolveBlock &solver_base) = delete;
+
+  /**
+   * @brief Copy assignment.
+   *
+   * Deleted so solver instances aren't copied.
+   */
+  SolveBlock &
+  operator=(const SolveBlock &solver_base) = delete;
+
+  /**
+   * @brief Move constructor.
+   *
+   * Deleted so solver instances aren't moved.
+   */
+  SolveBlock(SolveBlock &&solver_base) noexcept = delete;
+
+  /**
+   * @brief Move assignment.
+   *
+   * Deleted so solver instances aren't moved.
+   */
+  SolveBlock &
+  operator=(SolveBlock &&solver_base) noexcept = delete;
+
+  /**
+   * @brief Initialize all of the solvers.
+   */
+  void
+  init();
+
+  /**
+   * @brief Reinitialize all of the solvers.
+   */
+  void
+  reinit();
+
+  /**
+   * @brief Solve a single incremenet for all of the solvers.
+   *
+   * TODO (landinjm): Fix the logic here with the passed variables
+   */
+  void
+  solve(unsigned int increment, bool update_postprocessed);
+
+private:
+  /**
+   * @brief Explicit constant field solver class.
+   */
+  ConcurrentConstantSolver<dim, degree, number> concurrent_constant_solver;
+
+  /**
+   * @brief Explicit field solver class.
+   */
+  ConcurrentExplicitSolver<dim, degree, number> concurrent_explicit_solver;
+
+  /**
+   * @brief Postprocessed explicit field solver class.
+   */
+  ConcurrentExplicitPostprocessSolver<dim, degree, number>
+    concurrent_explicit_postprocess_solver;
+
+  /**
+   * @brief Nonexplicit auxiliary field solver class.
+   */
+  SequentialAuxiliarySolver<dim, degree, number> sequential_auxiliary_solver;
+
+  /**
+   * @brief Nonexplicit linear field solver class.
+   */
+  SequentialLinearSolver<dim, degree, number> sequential_linear_solver;
+
+  /**
+   * @brief Nonexplicit self-nonlinear field solver class.
+   */
+  SequentialSelfNonlinearSolver<dim, degree, number> sequential_self_nonlinear_solver;
+
+  /**
+   * @brief Nonexplicit co-nonlinear field solver class.
+   */
+  SequentialCoNonlinearSolver<dim, degree, number> sequential_co_nonlinear_solver;
+};
+
 /**
  * @brief The class handles the initialization and solving of the various types of solvers
  * in PRISMS-PF.
@@ -95,49 +198,7 @@ private:
   /**
    * @brief Set of solve blocks that we have.
    */
-  std::set<Types::Index> solve_blocks;
-
-  /**
-   * @brief Explicit constant field solver class.
-   */
-  std::map<Types::Index, ConcurrentConstantSolver<dim, degree, number>>
-    concurrent_constant_solver;
-
-  /**
-   * @brief Explicit field solver class.
-   */
-  std::map<Types::Index, ConcurrentExplicitSolver<dim, degree, number>>
-    concurrent_explicit_solver;
-
-  /**
-   * @brief Postprocessed explicit field solver class.
-   */
-  std::map<Types::Index, ConcurrentExplicitPostprocessSolver<dim, degree, number>>
-    concurrent_explicit_postprocess_solver;
-
-  /**
-   * @brief Nonexplicit auxiliary field solver class.
-   */
-  std::map<Types::Index, SequentialAuxiliarySolver<dim, degree, number>>
-    sequential_auxiliary_solver;
-
-  /**
-   * @brief Nonexplicit linear field solver class.
-   */
-  std::map<Types::Index, SequentialLinearSolver<dim, degree, number>>
-    sequential_linear_solver;
-
-  /**
-   * @brief Nonexplicit self-nonlinear field solver class.
-   */
-  std::map<Types::Index, SequentialSelfNonlinearSolver<dim, degree, number>>
-    sequential_self_nonlinear_solver;
-
-  /**
-   * @brief Nonexplicit co-nonlinear field solver class.
-   */
-  std::map<Types::Index, SequentialCoNonlinearSolver<dim, degree, number>>
-    sequential_co_nonlinear_solver;
+  std::map<Types::Index, SolveBlock<dim, degree, number>> solve_blocks;
 };
 
 PRISMS_PF_END_NAMESPACE
--- src/core/solver_handler.cc
+++ src/core/solver_handler.cc
@@ -18,32 +18,142 @@
 PRISMS_PF_BEGIN_NAMESPACE
 
 template <unsigned int dim, unsigned int degree, typename number>
+SolveBlock<dim, degree, number>::SolveBlock(
+  const SolverContext<dim, degree, number> &_solver_context,
+  Types::Index                              block_index)
+  : concurrent_constant_solver(_solver_context, block_index)
+  , concurrent_explicit_solver(_solver_context, block_index)
+  , concurrent_explicit_postprocess_solver(_solver_context, block_index)
+  , sequential_auxiliary_solver(_solver_context, block_index)
+  , sequential_linear_solver(_solver_context, block_index)
+  , sequential_self_nonlinear_solver(_solver_context, block_index)
+  , sequential_co_nonlinear_solver(_solver_context, block_index)
+{}
+
+template <unsigned int dim, unsigned int degree, typename number>
+void
+SolveBlock<dim, degree, number>::init()
+{
+  concurrent_constant_solver.init();
+
+  concurrent_explicit_solver.init();
+
+  concurrent_explicit_postprocess_solver.init();
+
+  sequential_auxiliary_solver.init();
+
+  sequential_linear_solver.init();
+
+  sequential_self_nonlinear_solver.init();
+
+  sequential_co_nonlinear_solver.init();
+}
+
+template <unsigned int dim, unsigned int degree, typename number>
+void
+SolveBlock<dim, degree, number>::reinit()
+{
+  concurrent_constant_solver.reinit();
+
+  concurrent_explicit_solver.reinit();
+
+  concurrent_explicit_postprocess_solver.reinit();
+
+  sequential_auxiliary_solver.reinit();
+
+  sequential_linear_solver.reinit();
+
+  sequential_self_nonlinear_solver.reinit();
+
+  sequential_co_nonlinear_solver.reinit();
+}
+
+template <unsigned int dim, unsigned int degree, typename number>
+void
+SolveBlock<dim, degree, number>::solve(unsigned int increment, bool update_postprocessed)
+{
+  if (increment == 0)
+    {
+      // Solve the auxiliary fields at the 0th step
+      ConditionalOStreams::pout_base() << "  solving auxiliary variables...\n"
+                                       << std::flush;
+      Timer::start_section("Auxiliary solver");
+      sequential_auxiliary_solver.solve();
+      Timer::end_section("Auxiliary solver");
+
+      // Solve the linear time-independent fields at the 0th step
+      ConditionalOStreams::pout_base()
+        << "  solving linear time-independent variables...\n"
+        << std::flush;
+      Timer::start_section("Nonexplicit linear solver");
+      sequential_linear_solver.solve();
+      Timer::end_section("Nonexplicit linear solver");
+
+      // Solve the self-nonlinear time-independent fields at the 0th step
+      ConditionalOStreams::pout_base()
+        << "  solving self-nonlinear time-independent variables...\n"
+        << std::flush;
+      Timer::start_section("Nonexplicit self-nonlinear solver");
+      sequential_self_nonlinear_solver.solve();
+      Timer::end_section("Nonexplicit self-nonlinear solver");
+
+      // Solve the co-nonlinear time-independent fields at the 0th step
+      ConditionalOStreams::pout_base()
+        << "  solving co-nonlinear time-independent variables...\n"
+        << std::flush;
+      Timer::start_section("Nonexplicit co-nonlinear solver");
+      sequential_co_nonlinear_solver.solve();
+      Timer::end_section("Nonexplicit co-nonlinear solver");
+
+      // Solve the postprocessed fields at the 0th step
+      ConditionalOStreams::pout_base() << "  solving postprocessed variables...\n"
+                                       << std::flush;
+      Timer::start_section("Postprocess solver");
+      concurrent_explicit_postprocess_solver.solve();
+      Timer::end_section("Postprocess solver");
+
+      return;
+    }
+
+  Timer::start_section("Explicit solver");
+  concurrent_explicit_solver.solve();
+  Timer::end_section("Explicit solver");
+
+  Timer::start_section("Nonexplicit auxiliary solver");
+  sequential_auxiliary_solver.solve();
+  Timer::end_section("Nonexplicit auxiliary solver");
+
+  Timer::start_section("Nonexplicit linear solver");
+  sequential_linear_solver.solve();
+  Timer::end_section("Nonexplicit linear solver");
+
+  Timer::start_section("Nonexplicit self-nonlinear solver");
+  sequential_self_nonlinear_solver.solve();
+  Timer::end_section("Nonexplicit self-nonlinear solver");
+
+  Timer::start_section("Nonexplicit co-nonlinear solver");
+  sequential_co_nonlinear_solver.solve();
+  Timer::end_section("Nonexplicit co-nonlinear solver");
+
+  if (update_postprocessed)
+    {
+      Timer::start_section("Postprocess solver");
+      concurrent_explicit_postprocess_solver.solve();
+      Timer::end_section("Postprocess solver");
+    }
+}
+
+template <unsigned int dim, unsigned int degree, typename number>
 SolverHandler<dim, degree, number>::SolverHandler(
   const SolverContext<dim, degree, number> &_solver_context)
 {
   // Create a set of the solve blocks
-  for (const auto &[index, variable] :
+  for (const auto &[var_index, variable] :
        _solver_context.get_user_inputs().get_variable_attributes())
     {
-      solve_blocks.insert(variable.get_solve_block());
-    }
-
-  // Create the map of solvers according to each solve block
-  for (const auto &solve_block : solve_blocks)
-    {
-      concurrent_constant_solver.try_emplace(solve_block, _solver_context, solve_block);
-      concurrent_explicit_solver.try_emplace(solve_block, _solver_context, solve_block);
-      concurrent_explicit_postprocess_solver.try_emplace(solve_block,
-                                                         _solver_context,
-                                                         solve_block);
-      sequential_auxiliary_solver.try_emplace(solve_block, _solver_context, solve_block);
-      sequential_linear_solver.try_emplace(solve_block, _solver_context, solve_block);
-      sequential_self_nonlinear_solver.try_emplace(solve_block,
-                                                   _solver_context,
-                                                   solve_block);
-      sequential_co_nonlinear_solver.try_emplace(solve_block,
-                                                 _solver_context,
-                                                 solve_block);
+      solve_blocks.try_emplace(variable.get_solve_block(),
+                               _solver_context,
+                               variable.get_solve_block());
     }
 }
 
@@ -53,46 +163,12 @@ SolverHandler<dim, degree, number>::init()
 {
   Timer::start_section("Solver initialization");
 
-  for (const auto &solve_block : solve_blocks)
+  for (auto &[block_index, solve_block] : solve_blocks)
     {
-      ConditionalOStreams::pout_base() << "initializing solvers for solve block "
-                                       << std::to_string(solve_block) << " ...\n"
+      ConditionalOStreams::pout_base() << "Initializing solvers for solve block "
+                                       << std::to_string(block_index) << " ...\n"
                                        << std::flush;
-
-      ConditionalOStreams::pout_base()
-        << "  trying to initialize concurrent constant solvers...\n"
-        << std::flush;
-      concurrent_constant_solver.at(solve_block).init();
-
-      ConditionalOStreams::pout_base()
-        << "  trying to initialize concurrent explicit solvers...\n"
-        << std::flush;
-      concurrent_explicit_solver.at(solve_block).init();
-
-      ConditionalOStreams::pout_base()
-        << "  trying to initialize concurrent explicit postprocess solvers...\n"
-        << std::flush;
-      concurrent_explicit_postprocess_solver.at(solve_block).init();
-
-      ConditionalOStreams::pout_base()
-        << "  trying to initialize sequential auxiliary solvers...\n"
-        << std::flush;
-      sequential_auxiliary_solver.at(solve_block).init();
-
-      ConditionalOStreams::pout_base()
-        << "  trying to initialize sequential linear solvers...\n"
-        << std::flush;
-      sequential_linear_solver.at(solve_block).init();
-
-      ConditionalOStreams::pout_base()
-        << "  trying to initialize sequential self-nonlinear solvers...\n"
-        << std::flush;
-      sequential_self_nonlinear_solver.at(solve_block).init();
-
-      ConditionalOStreams::pout_base()
-        << "  trying to initialize sequential co-nonlinear solvers...\n"
-        << std::flush;
-      sequential_co_nonlinear_solver.at(solve_block).init();
+      solve_block.init();
     }
 
   Timer::end_section("Solver initialization");
@@ -104,21 +180,9 @@ SolverHandler<dim, degree, number>::reinit()
 {
   Timer::start_section("Solver reinitialization");
 
-  for (const auto &solve_block : solve_blocks)
+  for (auto &[block_index, solve_block] : solve_blocks)
     {
-      concurrent_constant_solver.at(solve_block).reinit();
-
-      concurrent_explicit_solver.at(solve_block).reinit();
-
-      concurrent_explicit_postprocess_solver.at(solve_block).reinit();
-
-      sequential_auxiliary_solver.at(solve_block).reinit();
-
-      sequential_linear_solver.at(solve_block).reinit();
-
-      sequential_self_nonlinear_solver.at(solve_block).reinit();
-
-      sequential_co_nonlinear_solver.at(solve_block).reinit();
+      solve_block.reinit();
     }
 
   Timer::end_section("Solver reinitialization");
@@ -131,85 +195,20 @@ SolverHandler<dim, degree, number>::solve(unsigned int increment,
 {
   if (increment == 0)
     {
-      for (const auto &solve_block : solve_blocks)
+      for (auto &[block_index, solve_block] : solve_blocks)
         {
           ConditionalOStreams::pout_base()
-            << "solving 0th timestep for solve block " << std::to_string(solve_block)
+            << "solving 0th timestep for solve block " << std::to_string(block_index)
             << " ...\n " << std::flush;
-
-          // Solve the auxiliary fields at the 0th step
-          ConditionalOStreams::pout_base() << "  solving auxiliary variables...\n"
-                                           << std::flush;
-          Timer::start_section("Auxiliary solver");
-          sequential_auxiliary_solver.at(solve_block).solve();
-          Timer::end_section("Auxiliary solver");
-
-          // Solve the linear time-independent fields at the 0th step
-          ConditionalOStreams::pout_base()
-            << "  solving linear time-independent variables...\n"
-            << std::flush;
-          Timer::start_section("Nonexplicit linear solver");
-          sequential_linear_solver.at(solve_block).solve();
-          Timer::end_section("Nonexplicit linear solver");
-
-          // Solve the self-nonlinear time-independent fields at the 0th step
-          ConditionalOStreams::pout_base()
-            << "  solving self-nonlinear time-independent variables...\n"
-            << std::flush;
-          Timer::start_section("Nonexplicit self-nonlinear solver");
-          sequential_self_nonlinear_solver.at(solve_block).solve();
-          Timer::end_section("Nonexplicit self-nonlinear solver");
-
-          // Solve the co-nonlinear time-independent fields at the 0th step
-          ConditionalOStreams::pout_base()
-            << "  solving co-nonlinear time-independent variables...\n"
-            << std::flush;
-          Timer::start_section("Nonexplicit co-nonlinear solver");
-          sequential_co_nonlinear_solver.at(solve_block).solve();
-          Timer::end_section("Nonexplicit co-nonlinear solver");
-
-          if (update_postprocessed)
-            {
-              // Solve the postprocessed fields at the 0th step
-              ConditionalOStreams::pout_base() << "  solving postprocessed variables...\n"
-                                               << std::flush;
-              Timer::start_section("Postprocess solver");
-              concurrent_explicit_postprocess_solver.at(solve_block).solve();
-              Timer::end_section("Postprocess solver");
-            }
+          solve_block.solve(increment, update_postprocessed);
         }
       return;
     }
 
-  for (const auto &solve_block : solve_blocks)
+  for (auto &[block_index, solve_block] : solve_blocks)
     {
-      Timer::start_section("Explicit solver");
-      concurrent_explicit_solver.at(solve_block).solve();
-      Timer::end_section("Explicit solver");
-
-      Timer::start_section("Nonexplicit auxiliary solver");
-      sequential_auxiliary_solver.at(solve_block).solve();
-      Timer::end_section("Nonexplicit auxiliary solver");
-
-      Timer::start_section("Nonexplicit linear solver");
-      sequential_linear_solver.at(solve_block).solve();
-      Timer::end_section("Nonexplicit linear solver");
-
-      Timer::start_section("Nonexplicit self-nonlinear solver");
-      sequential_self_nonlinear_solver.at(solve_block).solve();
-      Timer::end_section("Nonexplicit self-nonlinear solver");
-
-      Timer::start_section("Nonexplicit co-nonlinear solver");
-      sequential_co_nonlinear_solver.at(solve_block).solve();
-      Timer::end_section("Nonexplicit co-nonlinear solver");
-
-      if (update_postprocessed)
-        {
-          Timer::start_section("Postprocess solver");
-          concurrent_explicit_postprocess_solver.at(solve_block).solve();
-          Timer::end_section("Postprocess solver");
-        }
-    };
+      solve_block.solve(increment, update_postprocessed);
+    }
 }
 
 #include "core/solver_handler.inst"
