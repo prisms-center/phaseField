19290d14d43b3458b3b5af7624e9bb28f4837f40
Xander <102053371+fractalsbyx@users.noreply.github.com>
2025-12-17 21:21:04 -0500

    solution indexer

---
+++ include/prismspf/core/solution_indexer.h
@@ -0,0 +1,67 @@
+// SPDX-FileCopyrightText: © 2025 PRISMS Center at the University of Michigan
+// SPDX-License-Identifier: GNU Lesser General Public Version 2.1
+
+#pragma once
+
+#include <prismspf/core/group_solution_handler.h>
+
+#include <prismspf/config.h>
+
+#include <vector>
+
+PRISMS_PF_BEGIN_NAMESPACE
+
+/**
+ * @brief Class that provides const access to solution vectors spread across different
+ * groups.
+ */
+template <unsigned int dim, typename number>
+class SolutionIndexer
+{
+public:
+  using BlockVector    = dealii::LinearAlgebra::distributed::BlockVector<number>;
+  using SolutionVector = BlockVector::BlockType;
+#if DEAL_II_VERSION_MAJOR >= 9 && DEAL_II_VERSION_MINOR >= 7
+  using SolutionTransfer = dealii::SolutionTransfer<dim, BlockVector>;
+#else
+  using SolutionTransfer =
+    dealii::parallel::distributed::SolutionTransfer<dim, BlockVector>;
+#endif
+
+  /**
+   * @brief Constructor.
+   */
+  SolutionIndexer(unsigned int num_fields, // attributes_list.size()
+                  const std::set<GroupSolutionHandler<dim, number>> &solution_handlers);
+
+  /**
+   * @brief Get a solution vector of a given field index.
+   */
+  [[nodiscard]] const SolutionVector &
+  get_solution_vector(unsigned int global_index, unsigned int relative_level = 0);
+
+  /**
+   * @brief Get a solution vector of a given field index at a given age.
+   */
+  [[nodiscard]] const SolutionVector &
+  get_old_solution_vector(unsigned int age,
+                          unsigned int global_index,
+                          unsigned int relative_level = 0);
+
+  /**
+   * @brief Get the "new" solution vector set.
+   */
+  [[nodiscard]] const BlockVector &
+  get_new_solution_full_vector(unsigned int relative_level = 0);
+
+  /**
+   * @brief Get the "new" solution vector of a given field index.
+   */
+  [[nodiscard]] const SolutionVector &
+  get_new_solution_vector(unsigned int index, unsigned int relative_level = 0);
+
+private:
+  std::vector<const GroupSolutionHandler<dim, number> *> solutions;
+};
+
+PRISMS_PF_END_NAMESPACE
