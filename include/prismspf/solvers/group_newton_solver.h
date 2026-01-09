46df39476c3567e79a7b33e8f1ee2909aefaf961
Xander <102053371+fractalsbyx@users.noreply.github.com>
2026-01-09 15:50:13 -0500

    moved solve implementation to headers for my sanity

--- include/prismspf/solvers/group_newton_solver.h
+++ include/prismspf/solvers/group_newton_solver.h
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#include <deal.II/lac/solver_cg.h>
+
 #include <prismspf/core/types.h>
 
 #include <prismspf/solvers/group_linear_solver.h>
@@ -38,57 +40,79 @@ public:
   {}
 
   /**
-   * @brief Destructor.
-   */
-  ~NewtonSolver() override = default;
-
-  /**
-   * @brief Copy constructor.
-   *
-   * Deleted so solver instances aren't copied.
-   */
-  NewtonSolver(const NewtonSolver &solver_base) = delete;
-
-  /**
-   * @brief Copy assignment.
-   *
-   * Deleted so solver instances aren't copied.
-   */
-  NewtonSolver &
-  operator=(const NewtonSolver &solver_base) = delete;
-
-  /**
-   * @brief Move constructor.
-   *
-   * Deleted so solver instances aren't moved.
-   */
-  NewtonSolver(NewtonSolver &&solver_base) noexcept = delete;
-
-  /**
-   * @brief Move assignment.
-   *
-   * Deleted so solver instances aren't moved.
-   */
-  NewtonSolver &
-  operator=(NewtonSolver &&solver_base) noexcept = delete;
-
-  /**
    * @brief Initialize the solver.
    */
   void
-  init() override;
+  init() override
+  {
+    for (unsigned int relative_level = 0; relative_level < mf_operators.size();
+         ++relative_level)
+      {
+        mf_operators[relative_level] = MFOperator<dim, degree, number>(
+          solver_context->pde_operator,
+          PDEOperator<dim, degree, number>::compute_explicit_rhs,
+          solver_context->field_attributes,
+          solver_context->solution_indexer,
+          relative_level,
+          solve_group.dependencies_rhs);
+      }
+    reinit();
+  }
 
   /**
    * @brief Reinitialize the solver.
    */
   void
-  reinit() override;
+  reinit() override
+  {
+    for (unsigned int relative_level = 0; relative_level < mf_operators.size();
+         ++relative_level)
+      {
+        mf_operators[relative_level].initialize(solve_group,
+                                                solutions.get_matrix_free(relative_level),
+                                                solutions.get_global_to_block_index());
+      }
+  }
 
   /**
    * @brief Solve for a single update step.
    */
   void
-  solve_level(unsigned int relative_level) override;
+  solve_level(unsigned int relative_level) override
+  {
+    number       step_length        = number(1.0); // TODO
+    number       tolerance          = number(1.0); // TODO
+    unsigned int max_iterations     = 1;           // TODO
+    bool         newton_unconverged = true;
+    unsigned int iter               = 0;
+
+    // TODO: setup initial guess for solution vector. Maybe use old_solution if available.
+    // Newton iteration loop.
+    while (newton_unconverged && iter++ < max_iterations)
+      {
+        // Solve for Newton update.
+        LinearSolver<dim, degree, number>::solve_level(relative_level);
+
+        // TODO: Apply zero-constraints to 'change' vector
+
+        // Perform Newton update.
+        solutions.get_solution_full_vector(relative_level)
+          .add(step_length, solutions.get_change_solution_full_vector(relative_level));
+
+        // TODO: Apply constraints to solution vector
+
+        // Check convergence.
+        number l2_norm =
+          solutions.get_change_solution_full_vector(relative_level).l2_norm();
+        newton_unconverged = l2_norm > tolerance;
+      }
+    ConditionalOStreams::pout_verbose() << iter << " Newton iterations to converge.\n\n";
+    if (iter >= max_iterations)
+      {
+        ConditionalOStreams::pout_base() << "Warning: nonlinear solver did not "
+                                            "converge as per set tolerances.\n\n";
+      }
+  }
 
   /**
    * @brief Solve for a single update step.
