ea847a607336b493b614faaf5c2fcbe27678b4fc
landinjm <landinjm@umich.edu>
2026-03-09 23:42:31 -0400

    temp

--- cmake/macros/macro_set_cpp_standard.cmake
+++
@@ -1,9 +0,0 @@
-#
-# Set the C++ standard
-#
-
-macro(set_cpp_standard _number)
-  set(CMAKE_CXX_STANDARD ${_number})
-  set(CMAKE_CXX_STANDARD_REQUIRED ON)
-  set(CMAKE_CXX_EXTENSIONS OFF)
-endmacro()
