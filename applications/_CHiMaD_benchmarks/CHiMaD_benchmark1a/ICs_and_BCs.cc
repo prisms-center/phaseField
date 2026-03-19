2acae068056caf12ccffdad539d45b4633dc0411
dhyuan-sys <dhyuan@umich.edu>
2026-03-19 12:30:04 -0400

    Updated app

--- applications/_CHiMaD_benchmarks/CHiMaD_benchmark1a/ICs_and_BCs.cc
+++ applications/_CHiMaD_benchmarks/CHiMaD_benchmark1a/ICs_and_BCs.cc
@@ -1,34 +1,36 @@
 // SPDX-FileCopyrightText: © 2025 PRISMS Center at the University of Michigan
 // SPDX-License-Identifier: GNU Lesser General Public Version 2.1
 
-// ===========================================================================
-// FUNCTION FOR INITIAL CONDITIONS
-// ===========================================================================
+#include "custom_pde.h"
 
-#include <deal.II/base/utilities.h>
+#include <prismspf/core/initial_conditions.h>
+#include <prismspf/core/nonuniform_dirichlet.h>
 
-template <int dim, int degree>
+#include <prismspf/user_inputs/user_input_parameters.h>
+
+#include <prismspf/config.h>
+
+#include <cmath>
+
+PRISMS_PF_BEGIN_NAMESPACE
+
+template <unsigned int dim, unsigned int degree, typename number>
 void
-CustomPDE<dim, degree>::setInitialCondition([[maybe_unused]] const Point<dim>  &p,
-                                            [[maybe_unused]] const unsigned int index,
-                                            [[maybe_unused]] number            &scalar_IC,
-                                            [[maybe_unused]] Vector<double>    &vector_IC)
+CustomPDE<dim, degree, number>::set_initial_condition(
+  [[maybe_unused]] const unsigned int       &index,
+  [[maybe_unused]] const unsigned int       &component,
+  [[maybe_unused]] const dealii::Point<dim> &point,
+  [[maybe_unused]] number                   &scalar_value,
+  [[maybe_unused]] number                   &vector_component_value) const
 {
-  // ---------------------------------------------------------------------
-  // ENTER THE INITIAL CONDITIONS HERE
-  // ---------------------------------------------------------------------
-  // Enter the function describing conditions for the fields at point "p".
-  // Use "if" statements to set the initial condition for each variable
-  // according to its variable index
-
-  if (index == 0)
+    if (index == 0)
     {
       double epsilon = 0.01;
       double c0      = 0.5;
-      double x       = p[0];
-      double y       = p[1];
+      double x       = point[0];
+      double y       = point[1];
 
-      scalar_IC =
+      scalar_value =
         c0 + epsilon * (std::cos(0.105 * x) * std::cos(0.11 * y) +
                         dealii::Utilities::fixed_power<2>(std::cos(0.13 * x) *
                                                           std::cos(0.087 * y)) +
@@ -36,37 +38,22 @@ CustomPDE<dim, degree>::setInitialCondition([[maybe_unused]] const Point<dim>  &
     }
   else
     {
-      scalar_IC = 0.0;
+      scalar_value = 0.0;
     }
-
-  // ---------------------------------------------------------------------
 }
 
-// ===========================================================================
-// FUNCTION FOR NON-UNIFORM Dirichlet BOUNDARY CONDITIONS
-// ===========================================================================
-
-template <int dim, int degree>
+template <unsigned int dim, unsigned int degree, typename number>
 void
-CustomPDE<dim, degree>::setNonUniformDirichletBCs(
-  [[maybe_unused]] const Point<dim>  &p,
-  [[maybe_unused]] const unsigned int index,
-  [[maybe_unused]] const unsigned int direction,
-  [[maybe_unused]] const number       time,
-  [[maybe_unused]] number            &scalar_BC,
-  [[maybe_unused]] Vector<double>    &vector_BC)
-{
-  // --------------------------------------------------------------------------
-  // ENTER THE NON-UNIFORM Dirichlet BOUNDARY CONDITIONS HERE
-  // --------------------------------------------------------------------------
-  // Enter the function describing conditions for the fields at point "p".
-  // Use "if" statements to set the boundary condition for each variable
-  // according to its variable index. This function can be left blank if there
-  // are no non-uniform Dirichlet boundary conditions. For BCs that change in
-  // time, you can access the current time through the variable "time". The
-  // boundary index can be accessed via the variable "direction", which starts
-  // at zero and uses the same order as the BC specification in parameters.in
-  // (i.e. left = 0, right = 1, bottom = 2, top = 3, front = 4, back = 5).
+CustomPDE<dim, degree, number>::set_nonuniform_dirichlet(
+  [[maybe_unused]] const unsigned int       &index,
+  [[maybe_unused]] const unsigned int       &boundary_id,
+  [[maybe_unused]] const unsigned int       &component,
+  [[maybe_unused]] const dealii::Point<dim> &point,
+  [[maybe_unused]] number                   &scalar_value,
+  [[maybe_unused]] number                   &vector_component_value) const
+{}
 
-  // -------------------------------------------------------------------------
-}
+#include "custom_pde.inst"
+
+PRISMS_PF_END_NAMESPACE
+
