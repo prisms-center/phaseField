8b915a3ae8426b7370430fa1fd9d39913790492e
Jason Landini <123970616+landinjm@users.noreply.github.com>
2026-03-27 16:10:04 -0400

    removing broken apps (#880)

--- tests/unit_tests/utilities/main.cc
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: © 2025 PRISMS Center at the University of Michigan
-// SPDX-License-Identifier: GNU Lesser General Public Version 2.1
-
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
