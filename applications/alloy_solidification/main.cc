084e1051f388ef86db56fbd36dcb8f5daf7c32a9
Jason Landini <123970616+landinjm@users.noreply.github.com>
2026-04-12 12:22:57 -0400

    some simplifiications, notes (#953)
    * some simplifiications, notes
    
    * precommit

--- applications/alloy_solidification/main.cc
+++ applications/alloy_solidification/main.cc
@@ -25,6 +25,20 @@ main(int argc, char *argv[])
   constexpr unsigned int dim    = 2;
   constexpr unsigned int degree = 2;
 
+  /**
+   * We have four fields in this application.
+   *   U - The dimensionless supersaturation
+   *   phi - The solid/liquid order parameter
+   *   xi - The auxiliary field used to split the order parameter evolution equation
+   *   c - The concentration.
+   *
+   * The first three equations are explicit with U and phi evolving with a forward Euler
+   * time integration scheme. The interesting particle of the equation is xi, which we use
+   * to make the evaluation of phi easier. This auxiliary field, xi, has no initial
+   * condition and is only used to evolve the order parameter.
+   *
+   * The last field is the concentration, which we only need for postprocessing.
+   */
   std::vector<FieldAttributes> fields = {FieldAttributes("U"),
                                          FieldAttributes("phi"),
                                          FieldAttributes("xi"),
