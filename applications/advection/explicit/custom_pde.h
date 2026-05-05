a68f9ad8f8123ee2c0ee879ca58511e8e29d3390
landinjm <landinjm@umich.edu>
2026-05-05 18:52:55 -0400

    tmp

---
+++ applications/advection/explicit/custom_pde.h
@@ -0,0 +1,134 @@
+// SPDX-FileCopyrightText: © 2025 PRISMS Center at the University of Michigan
+// SPDX-License-Identifier: GNU Lesser General Public Version 2.1
+
+#include <deal.II/base/function_signed_distance.h>
+
+#include <prismspf/core/pde_operator_base.h>
+
+#include <prismspf/utilities/utilities.h>
+
+PRISMS_PF_BEGIN_NAMESPACE
+
+template <unsigned int dim, unsigned int degree, typename number>
+class CustomPDE : public PDEOperatorBase<dim, degree, number>
+{
+public:
+  using ScalarValue = dealii::VectorizedArray<number>;
+  using ScalarGrad  = dealii::Tensor<1, dim, ScalarValue>;
+  using ScalarHess  = dealii::Tensor<2, dim, ScalarValue>;
+  using VectorValue = dealii::Tensor<1, dim, ScalarValue>;
+  using VectorGrad  = dealii::Tensor<2, dim, ScalarValue>;
+  using VectorHess  = dealii::Tensor<3, dim, ScalarValue>;
+  using PDEOperatorBase<dim, degree, number>::get_user_inputs;
+  using PDEOperatorBase<dim, degree, number>::get_pf_tools;
+
+  CustomPDE(const UserInputParameters<dim> &_user_inputs, PhaseFieldTools<dim> &_pf_tools)
+    : PDEOperatorBase<dim, degree, number>(_user_inputs, _pf_tools)
+    , W(_user_inputs.user_constants.get_double("W"))
+  {
+    // Compute the center of the domain, promoting the doubles to vectorized arrays
+    // NOTE: This assumes a rectangular mesh
+    for (unsigned int d = 0; d < dim; ++d)
+      {
+        domain_center[d] =
+          _user_inputs.spatial_discretization.rectangular_mesh.size[d] / 2.0;
+      }
+  };
+
+private:
+  void
+  set_initial_condition([[maybe_unused]] const unsigned int       &index,
+                        [[maybe_unused]] const unsigned int       &component,
+                        [[maybe_unused]] const dealii::Point<dim> &point,
+                        [[maybe_unused]] number                   &scalar_value,
+                        [[maybe_unused]] number &vector_component_value) const override
+  {
+    using std::tanh;
+
+    // Zalesak's disk level-set
+    // TODO: Refactor these
+    const double             radius       = 15.0;
+    const double             notch_width  = 5.0;
+    const double             notch_height = 25.0;
+    const dealii::Point<dim> center(50.0, 75.0);
+
+    dealii::Functions::SignedDistance::ZalesakDisk<dim> zalesak_disk(center,
+                                                                     radius,
+                                                                     notch_width,
+                                                                     notch_height);
+    // The distance is just the level-set value
+    const double distance = zalesak_disk.value(point);
+
+    // Apply tanh
+    scalar_value = 0.5 * (1.0 - tanh(distance / (std::numbers::sqrt2 * W)));
+  };
+
+  void
+  set_dirichlet([[maybe_unused]] const unsigned int       &index,
+                [[maybe_unused]] const unsigned int       &boundary_id,
+                [[maybe_unused]] const unsigned int       &component,
+                [[maybe_unused]] const dealii::Point<dim> &point,
+                [[maybe_unused]] number                   &scalar_value,
+                [[maybe_unused]] number &vector_component_value) const override {};
+
+  void
+  compute_rhs([[maybe_unused]] FieldContainer<dim, degree, number> &variable_list,
+              [[maybe_unused]] const SimulationTimer               &sim_timer,
+              [[maybe_unused]] unsigned int solve_group_id) const override
+  {
+    ScalarValue dt = sim_timer.get_timestep();
+
+    if (solve_group_id == 0)
+      {
+        ScalarValue u      = variable_list.template get_value<Scalar, OldOne>(0);
+        ScalarGrad  u_grad = variable_list.template get_gradient<Scalar, OldOne>(0);
+        ScalarValue h      = variable_list.get_element_volume();
+        dealii::Point<dim, ScalarValue> p = variable_list.get_q_point_location();
+
+        // Compute the rotational velocity based on the distance from the center of the
+        // domain
+        // TODO: This assumes 2D
+        ScalarGrad v;
+        v[0] = -(p[1] - domain_center[1]);
+        v[1] = p[0] - domain_center[0];
+
+        // Compute the stabilization parameter
+        ScalarValue tau = stabilization_parameter<dim, degree>(dt, h, v);
+
+        variable_list.set_value_term(0, u);
+        variable_list.set_gradient_term(0, v * dt * u);
+      }
+  };
+
+  void
+  compute_lhs([[maybe_unused]] FieldContainer<dim, degree, number> &variable_list,
+              [[maybe_unused]] const SimulationTimer               &sim_timer,
+              [[maybe_unused]] unsigned int solver_id) const override
+  {
+    ScalarValue dt = sim_timer.get_timestep();
+
+    if (solver_id == 0)
+      {
+        ScalarValue u = variable_list.template get_value<Scalar, LHS>(0);
+        ScalarValue h = variable_list.get_element_volume();
+        dealii::Point<dim, ScalarValue> p = variable_list.get_q_point_location();
+
+        // Compute the rotational velocity based on the distance from the center of the
+        // domain
+        // TODO: This assumes 2D
+        ScalarGrad v;
+        v[0] = -(p[1] - domain_center[1]);
+        v[1] = p[0] - domain_center[0];
+
+        // Compute the stabilization parameter
+        ScalarValue tau = stabilization_parameter<dim, degree>(dt, h, v);
+
+        variable_list.set_value_term(0, u);
+      }
+  };
+
+  number                          W;
+  dealii::Point<dim, ScalarValue> domain_center;
+};
+
+PRISMS_PF_END_NAMESPACE
---
+++ applications/advection/explicit/main.cc
@@ -0,0 +1,50 @@
+// SPDX-FileCopyrightText: © 2025 PRISMS Center at the University of Michigan
+// SPDX-License-Identifier: GNU Lesser General Public Version 2.1
+
+#include "custom_pde.h"
+
+#include <prismspf/core/dependencies.h>
+#include <prismspf/core/field_attributes.h>
+#include <prismspf/core/parse_cmd_options.h>
+#include <prismspf/core/problem.h>
+#include <prismspf/core/solve_block.h>
+
+using namespace prisms;
+
+int
+main(int argc, char *argv[])
+{
+  // Initialize MPI
+  prisms::MPIInitFinalize mpi_init(argc, argv);
+
+  // Parse the command line options (if there are any) to get the name of the input
+  // file
+  ParseCMDOptions cli_options(argc, argv);
+
+  constexpr unsigned int dim    = 2;
+  constexpr unsigned int degree = 1;
+
+  std::vector<FieldAttributes> fields = {FieldAttributes("u")};
+
+  SolveBlock u_block;
+  u_block.id               = 0;
+  u_block.solve_type       = Linear;
+  u_block.solve_timing     = Initialized;
+  u_block.field_indices    = {0};
+  u_block.dependencies_rhs = make_dependency_set(fields, {"old_1(u)", "grad(old_1(u))"});
+  u_block.dependencies_lhs = make_dependency_set(fields, {"u"});
+
+  std::vector<SolveBlock> solve_groups({u_block});
+
+  UserInputParameters<dim>       user_inputs(cli_options.get_parameters_filename());
+  PhaseFieldTools<dim>           pf_tools;
+  CustomPDE<dim, degree, double> pde_operator(user_inputs, pf_tools);
+  Problem<dim, degree, double>   problem(fields,
+                                         solve_groups,
+                                         user_inputs,
+                                         pf_tools,
+                                         pde_operator);
+  problem.solve();
+
+  return 0;
+}
