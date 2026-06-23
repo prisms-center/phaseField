1359f039d64a96c5f81261b93c4f80fc187bf00f
landinjm <landinjm@umich.edu>
2026-06-23 17:51:58 -0400

    bdf2 navier-stokes projection:

---
+++ applications/navier_stokes/main.cc
@@ -0,0 +1,83 @@
+// SPDX-FileCopyrightText: © 2025 PRISMS Center at the University of Michigan
+// SPDX-License-Identifier: GNU Lesser General Public Version 2.1
+
+#include "custom_pde.h"
+
+#include <prismspf/core/dependencies.h>
+#include <prismspf/core/field_attributes.h>
+#include <prismspf/core/parse_cmd_options.h>
+#include <prismspf/core/problem.h>
+#include <prismspf/core/solve_block.h>
+#include <prismspf/core/type_enums.h>
+
+using namespace prismspf;
+
+int
+main(int argc, char *argv[])
+{
+  // Initialize MPI
+  prismspf::MPIInitFinalize mpi_init(argc, argv);
+
+  // Parse the command line options (if there are any) to get the name of the input
+  // file
+  ParseCMDOptions cli_options(argc, argv);
+
+  constexpr unsigned int dim    = 2;
+  constexpr unsigned int degree = 2;
+
+  // TODO: Add documentation
+  std::vector<FieldAttributes> fields = {FieldAttributes("u", Vector),
+                                         FieldAttributes("u_star", Vector),
+                                         FieldAttributes("p"),
+                                         FieldAttributes("p_star"),
+                                         FieldAttributes("phi")};
+
+  SolveBlock extrapolation;
+  extrapolation.id            = 1;
+  extrapolation.field_indices = {1, 3}; // u_star & p_star
+  extrapolation.solve_type    = Explicit;
+  extrapolation.solve_timing  = Uninitialized;
+  extrapolation.dependencies_rhs =
+    make_dependency_set(fields,
+                        {"old_1(u)", "old_2(u)", "old_1(p)", "old_1(phi)", "old_2(phi)"});
+
+  SolveBlock diffusion;
+  diffusion.id            = 2;
+  diffusion.field_indices = {0}; // u
+  diffusion.solve_type    = Linear;
+  diffusion.solve_timing  = Uninitialized;
+  diffusion.dependencies_rhs =
+    make_dependency_set(fields, {"old_1(u)", "old_2(u)", "grad(p_star)"});
+  diffusion.dependencies_lhs =
+    make_dependency_set(fields, {"lhs(u)", "grad(lhs(u))", "u_star", "grad(u_star)"});
+
+  SolveBlock projection;
+  projection.id               = 3;
+  projection.field_indices    = {4}; // phi
+  projection.solve_type       = Linear;
+  projection.solve_timing     = Uninitialized;
+  projection.dependencies_rhs = make_dependency_set(fields, {"grad(u)"});
+  projection.dependencies_lhs = make_dependency_set(fields, {"grad(lhs(phi))"});
+
+  SolveBlock pressure_correction;
+  pressure_correction.id               = 4;
+  pressure_correction.field_indices    = {2}; // p
+  pressure_correction.solve_type       = Explicit;
+  pressure_correction.solve_timing     = Uninitialized;
+  pressure_correction.dependencies_rhs = make_dependency_set(fields, {"old_1(p)", "phi"});
+
+  std::vector<SolveBlock> solve_blocks(
+    {extrapolation, diffusion, projection, pressure_correction});
+
+  UserInputParameters<dim>       user_inputs(cli_options.get_parameters_filename());
+  PhaseFieldTools<dim>           pf_tools;
+  CustomPDE<dim, degree, double> pde_operator(user_inputs, pf_tools);
+  Problem<dim, degree, double>   problem(fields,
+                                         solve_blocks,
+                                         user_inputs,
+                                         pf_tools,
+                                         pde_operator);
+  problem.solve();
+
+  return 0;
+}
