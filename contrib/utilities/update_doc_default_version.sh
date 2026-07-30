857e3998a07db677085d03e70e190570c36bcbc7
landinjm <landinjm@umich.edu>
2026-07-30 19:01:48 -0400

    okie should be good to go

--- contrib/utilities/update_doc_default_version.sh
+++ contrib/utilities/update_doc_default_version.sh
@@ -9,24 +9,39 @@
 # ./contrib/utilities/update_doc_version_selector.sh FILE_PATH
 #
 
+REMOTE="upstream"
+BRANCH="gh-pages"
 FILE_PATH="$1"
 
-# Grab the latest version from the config.h file
-config_h="${1%doc/}include/prismspf/config.h"
-latest_version=$(sed -n 's/^#define PRISMS_PF_VERSION "\(.*\)"/\1/p' "$config_h" | tr -d ' \n')
-echo "Latest version found: $latest_version"
+# Find the files in the doxygen folder of the remote branch
+git fetch "$REMOTE" "$BRANCH"
+dirs=$(git ls-tree --name-only -d "$REMOTE/$BRANCH":doxygen | sort -rV)
+echo "Found directories under doxygen/:"
+echo "$dirs"
+echo ""
+
+# Get the most recent git tag
+latest_tag=$(git describe --tags --abbrev=0)
+latest_tag=${latest_tag#v}
+echo "Latest tag: $latest_tag"
+
+# Check that the folder exists on the remote branch (gh-pages)
+if ! echo "$dirs" | grep -Fxq "${latest_tag}"; then
+  echo "Error: '${latest_tag}' does not exist under doxygen/ on $REMOTE/$BRANCH"
+  exit 1
+fi
 
 # Create HTML
-cat << EOF > $FILE_PATH/index.html
+cat <<EOF >$FILE_PATH/index.html
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="refresh" content="0; url=$latest_version/index.html">
+    <meta http-equiv="refresh" content="0; url=$latest_tag/index.html">
     <title>Redirecting...</title>
 </head>
 <body>
-    <p>If you are not redirected automatically, <a href="$latest_version/index.html">click here</a>.</p>
+    <p>If you are not redirected automatically, <a href="$latest_tag/index.html">click here</a>.</p>
 </body>
 </html>
 EOF
