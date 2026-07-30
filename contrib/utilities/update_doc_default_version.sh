93ab724be3420b5fba278ae96d9954af6b975547
landinjm <landinjm@umich.edu>
2026-07-30 19:23:03 -0400

    hotfix

--- contrib/utilities/update_doc_default_version.sh
+++ contrib/utilities/update_doc_default_version.sh
@@ -10,24 +10,25 @@
 #
 
 REMOTE="origin"
-BRANCH="gh-pages"
+BASE_BRANCH="master"
+DOC_BRANCH="gh-pages"
 FILE_PATH="$1"
 
 # Find the files in the doxygen folder of the remote branch
-git fetch "$REMOTE" "$BRANCH"
-dirs=$(git ls-tree --name-only -d "$REMOTE/$BRANCH":doxygen | sort -rV)
+git fetch "$REMOTE" "$DOC_BRANCH"
+dirs=$(git ls-tree --name-only -d "$REMOTE/$DOC_BRANCH":doxygen | sort -rV)
 echo "Found directories under doxygen/:"
 echo "$dirs"
 echo ""
 
 # Get the most recent git tag
-latest_tag=$(git describe --tags --abbrev=0)
+latest_tag=$(git describe --tags --abbrev=0 "$REMOTE/$BASE_BRANCH")
 latest_tag=${latest_tag#v}
 echo "Latest tag: $latest_tag"
 
 # Check that the folder exists on the remote branch (gh-pages)
 if ! echo "$dirs" | grep -Fxq "${latest_tag}"; then
-  echo "Error: '${latest_tag}' does not exist under doxygen/ on $REMOTE/$BRANCH"
+  echo "Error: '${latest_tag}' does not exist under doxygen/ on $REMOTE/$DOC_BRANCH"
   exit 1
 fi
 
