77331e89691cfcb22f673471fcab3934836101cb
landinjm <landinjm@umich.edu>
2026-07-31 10:34:19 -0400

    some asserts

--- src/core/problem.cc
+++ src/core/problem.cc
@@ -19,6 +19,8 @@
 
 #include <prismspf/user_inputs/user_input_parameters.h>
 
+#include <prismspf/utilities/assert.h>
+
 #include <algorithm>
 #include <filesystem>
 
@@ -309,6 +311,15 @@ template <unsigned int dim, unsigned int degree, typename number>
 void
 Problem<dim, degree, number>::solve()
 {
+  const UserInputParameters<dim> &user_inputs = *user_inputs_ptr;
+
+  // TODO: Remove these asserts as the features/bugs are fixed
+  ASSERT(!user_inputs.spatial_discretization.has_adaptivity || dim != 1,
+         "AMR cannot be enable for 1D in deal.II 9.7.0 and below.");
+  ASSERT(!user_inputs.spatial_discretization.has_adaptivity ||
+           !has_multigrid(solve_blocks),
+         "AMR cannot be enabled when using multigrid preconditioners currently.");
+
   Timer::start_section("Problem Solve");
   // Print a warning if running in DEBUG mode
   ConditionalOStreams::pout_verbose()
@@ -335,9 +346,8 @@ Problem<dim, degree, number>::solve()
     << "================================================\n"
     << std::flush;
 
-  const UserInputParameters<dim> &user_inputs = *user_inputs_ptr;
-  const TemporalDiscretization   &time_info   = user_inputs.temporal_discretization;
-  SimulationTimer                &sim_timer   = solve_context.get_simulation_timer();
+  const TemporalDiscretization &time_info = user_inputs.temporal_discretization;
+  SimulationTimer              &sim_timer = solve_context.get_simulation_timer();
   // Main time-stepping loop
   int exit_status = 0;
   while (sim_timer.get_increment() <= time_info.n_increments && exit_status == 0)
