397ff453981092a81f2590d34be382b92b0b3081
Philip Staublin <staublin@umich.edu>
2026-09-08 11:30:50 -0400

    Fix incorrect labeling of boundaries in RectangularMesh::Boundary (#1331)
    * Fix incorrect labeling of boundaries in RectangularMesh::Boundary

--- include/prismspf/user_inputs/spatial_discretization.h
+++ include/prismspf/user_inputs/spatial_discretization.h
@@ -149,8 +149,8 @@ struct RectangularMesh : public Mesh<dim>
   {
     Left   = 0,
     Right  = 1,
-    Top    = 2,
-    Bottom = 3,
+    Bottom = 2,
+    Top    = 3,
     Front  = 4,
     Back   = 5
   };
