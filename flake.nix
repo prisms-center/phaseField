c58aa35b02f4452fcdcc82830b25595f8a5e7386
Jason Landini <landinjm@umich.edu>
2026-09-16 11:49:40 -0400

    precommit and nix dev stuff

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "C++ partial differential equation framework";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            pre-commit
+          ];
+        };
+      };
+    };
+}
