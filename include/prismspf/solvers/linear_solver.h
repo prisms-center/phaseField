04b26848594a627dda52a860e81c07230d038249
Jason Landini <landinjm@umich.edu>
2026-09-16 13:12:57 -0400

    9.7.0 fices

--- include/prismspf/solvers/linear_solver.h
+++ include/prismspf/solvers/linear_solver.h
@@ -55,7 +55,12 @@ public:
   using Smoother        = dealii::MGSmootherPrecondition<MFOperator<dim, degree, number>,
                                                          SmootherPrecond,
                                                          BlockVector<number>>;
-  using MGTransferType  = dealii::MGTransferBlockMatrixFree<dim, BlockVector<number>>;
+#ifdef DEAL_II_VERSION_GTE(9, 8, 0)
+  using MGTransferType = dealii::MGTransferBlockMatrixFree<dim, BlockVector<number>>;
+#else
+  using MGTransferType =
+    dealii::MGTransferBlockGlobalCoarsening<dim, BlockVector<number>>;
+#endif
 
   // dc = default constructible, ndc = not default constructible
   dealii::MGLevelObject<MFOperator<dim, degree, number>> mg_lhs_operators; // dc
