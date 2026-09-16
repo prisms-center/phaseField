c90a0ec3275e09214a94427da3db63d0e9c3a4b5
Jason Landini <123970616+landinjm@users.noreply.github.com>
2026-09-16 12:06:11 -0400

    Fix deal.II link targets (#1346)
    * fix dealii targets
    
    * precommit and nix dev stuff

--- cmake/deps/dealii.cmake
+++ cmake/deps/dealii.cmake
@@ -86,6 +86,11 @@ if(DEAL_II_BUILD_TYPE STREQUAL "DebugRelease")
     prisms_pf_add_dependency_target(dealii::dealii_release RELEASE PUBLIC)
   endif()
 else()
-  prisms_pf_add_dependency_target(dealii::dealii DEBUG PUBLIC)
-  prisms_pf_add_dependency_target(dealii::dealii RELEASE PUBLIC)
+  # NOTE: For whatever reason, the dealii::dealii target is just an interface
+  # library for deal.II 9.8.0. Because of this, linking to it doesn't do
+  # anything. Instead, we have to grab the build type and determine
+  # whether we want dealii::dealii_release or dealii::dealii_debug
+  string(TOLOWER "${DEAL_II_BUILD_TYPE}" _dealii_build)
+  prisms_pf_add_dependency_target("dealii::dealii_${_dealii_build}" DEBUG PUBLIC)
+  prisms_pf_add_dependency_target("dealii::dealii_${_dealii_build}" RELEASE PUBLIC)
 endif()
