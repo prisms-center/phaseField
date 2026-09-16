2ce12cec1e29b961d9e67d407ae03f2cc238466f
Xander <102053371+fractalsbyx@users.noreply.github.com>
2026-09-16 14:08:06 -0400

    clang-format

--- include/prismspf/solvers/mf_operator.h
+++ include/prismspf/solvers/mf_operator.h
@@ -78,14 +78,14 @@ public:
     else
       {
         static Value<Rank> ident = []()
-          {
-            Value<Rank> obj;
-            for (int i = 0; i < Value<Rank>::n_independent_components; ++i)
-              {
-                obj[Value<Rank>::unrolled_to_component_indices(i)] = 1.0;
-              }
-            return obj;
-          }();
+        {
+          Value<Rank> obj;
+          for (int i = 0; i < Value<Rank>::n_independent_components; ++i)
+            {
+              obj[Value<Rank>::unrolled_to_component_indices(i)] = 1.0;
+            }
+          return obj;
+        }();
         return ident;
       }
   }
