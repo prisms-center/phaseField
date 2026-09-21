aaa1ef3ce60658bea6fdf1a9e801c034fb81c59d
Jason Landini <landinjm@umich.edu>
2026-09-21 12:13:42 -0400

    updating nix dev environment

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    dealii.url = "git+https://codeberg.org/landinjm/dealii-flake.git";
   };
 
   outputs = {flake-parts, ...} @ inputs:
@@ -15,10 +17,36 @@
         "x86_64-linux"
       ];
 
-      perSystem = {pkgs, ...}: {
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: let
+        config = {
+          allowUnfree = true;
+          cpuArch = "NATIVE";
+          cudaSupport = false;
+          cudaArch = "ADA89";
+          rocmSupport = false;
+          rocmArch = "";
+        };
+      in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            # Main requirements
+            cmake
+            gnumake
+            ninja
+            gcc
+            inputs.dealii.packages.${system}.default
+
+            # Pre-commit
+            llvmPackages_18.clang-tools
             pre-commit
+
+            # Documentation
+            doxygen
+            graphviz
           ];
         };
       };
